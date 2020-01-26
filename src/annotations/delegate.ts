7a6734b5ebc315026685abddb3a4d3f8d9b12610
Adam Desivi <desivi@desivi.com>
2020-01-26 17:12:06 +0100

    feat(annotations): add delegating annotation for types with property initializers

---
+++ src/annotations/delegate.ts
@@ -0,0 +1,20 @@
+import { DELEGATED_KEY } from '../constants/metadata-keys';
+
+/*
+Force delegated construction always on each Configuration and inheritables by
+requiring to define class constructor::
+
+class MyConfig extends Config {
+  constructor(props?: Partial<MyConfig>) {
+    super();
+    if (props) Object.assign(this, this.processProps(props));
+  }
+}
+
+So property initializers are ALWAYS supported by default in such way.
+*/
+export function delegate() {
+  return (target: Object) => {
+    Reflect.defineMetadata(DELEGATED_KEY, true, target);
+  };
+}
