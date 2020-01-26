bc34219c487f89f62b399a39061df3e00875647a
Adam Desivi <desivi@desivi.com>
2020-01-26 18:06:50 +0100

    feat(mixins): add CommandHandlingMixin

---
+++ src/mixins/command-handling-mixin.ts
@@ -0,0 +1,66 @@
+import { postConstruct, inject } from '@parisholley/inversify-async';
+import { OneToOneHandlingMixin } from './one-to-one-handling-mixin';
+import { types } from '../types';
+import { Command } from '../components/command';
+import { BINDINGS } from '../constants/bindings';
+
+export class CommandHandlingMixin extends OneToOneHandlingMixin
+  implements types.Sender {
+  @inject(BINDINGS.CommandBus)
+  public commandBus: types.Sender;
+
+  /**
+   * Initializes CommandHandlingMixin.
+   * @remarks
+   * By defining handler mappings on metadata with annotations: `@handle`
+   * and resolving those mapping with `handles` method - it ensures, that there
+   * is no leakage in between inheriting classes.
+   */
+  @postConstruct()
+  public initialize(): void {
+    this.setupHandlers({
+      handlers: this.handles(),
+      registrator: this.registerCommandHandler.bind(this),
+      handleableTypes: [Command],
+    });
+  }
+
+  /**
+   * Registers command handler and registers that command on command bus.
+   * @param commandType - A `Command` type for which handler mapping will be created.
+   * @param handler - Handler function that will executed upon handling type.
+   * @param shouldOverride - Flag indicating that handler should be overridden if exist.
+   * @remarks
+   * Ensure first that handler for command is registrable(i.e. only can
+   * handler can exist for specific Command on whole application)
+   * @throws {UnhandleableTypeError}
+   * Thrown if the type argument is not one of handleable types.
+   * @throws {InvalidHandlerError}
+   * Thrown if the handler argument is not a function.
+   * @throws {HandlerExistError}
+   * Thrown if handler would overridden without explicit call.
+   */
+  public registerCommandHandler(
+    commandType: types.MessageableType,
+    handler: types.Handler,
+    shouldOverride = false
+  ): void {
+    this.ensureHandleability(commandType, [Command]);
+
+    const boundHandler = handler.bind(this);
+    boundHandler.original = handler;
+    this.commandBus.registerHandler(commandType, boundHandler, shouldOverride);
+    this.registerHandler(commandType, boundHandler, shouldOverride);
+  }
+
+  /**
+   * Sends(handles) command instance through command bus.
+   * @async
+   * @param commandInstance - Instance of a `Command` type.
+   * @returns Any value that is returned from handler after handling command instance.
+   */
+  public async send(commandInstance: Command): Promise<any> {
+    const result = await this.commandBus.send(commandInstance);
+    return result;
+  }
+}
---
+++ test/unit/mixins/command-handling-mixin.test.ts
@@ -0,0 +1,229 @@
+import chai, { expect } from 'chai';
+import sinon from 'sinon';
+import sinonChai from 'sinon-chai';
+import { stubInterface } from 'ts-sinon';
+import { CommandHandlingMixin } from '../../../src/mixins/command-handling-mixin';
+import { OneToOneHandlingMixin } from '../../../src/mixins/one-to-one-handling-mixin';
+import { define } from '../../../src/decorators/define';
+import { Command } from '../../../src/components/command';
+import { Event } from '../../../src/components/event';
+import { handle } from '../../../src/annotations/handle';
+import { subscribe } from '../../../src/annotations/subscribe';
+import { types } from '../../../src/types';
+import { Container } from '../../../src/core/injector';
+import { BINDINGS } from '../../../src/constants/bindings';
+import { UnhandleableTypeError } from '../../../src/messaging/messaging-errors';
+
+chai.use(sinonChai);
+
+describe(`CommandHandlingMixin`, function() {
+  let container: types.Injector;
+  let commandBus: any;
+
+  beforeEach(() => {
+    commandBus = stubInterface<types.CommandBus>();
+    container = new Container();
+    container
+      .bind<types.CommandBus>(BINDINGS.CommandBus)
+      .toConstantValue(commandBus);
+  });
+
+  @define('MyCommand', { isRegistrable: false })
+  class MyCommand extends Command {
+    key: string;
+  }
+
+  @define('MyOtherCommand', { isRegistrable: false })
+  class MyOtherCommand extends Command {
+    key: string;
+  }
+
+  @define('MyEvent', { isRegistrable: false })
+  class MyEvent extends Event {
+    key: string;
+  }
+
+  it(`has OneToOneHandlingMixin mixin on prototype chain applied`, () => {
+    expect(CommandHandlingMixin.prototype).to.be.instanceof(
+      OneToOneHandlingMixin
+    );
+  });
+
+  describe('construction', () => {
+    it(`initializes with empty handled commands`, () => {
+      class MyController extends CommandHandlingMixin {}
+
+      const controller = new MyController();
+      expect(controller.getHandledCommands()).to.be.eql([]);
+    });
+  });
+
+  describe('initialization', () => {
+    it('sets handler for commands with dedicated registration method on initialization', () => {
+      class MyController extends CommandHandlingMixin {
+        @handle()
+        MyCommand(command: MyCommand): boolean {
+          return command.key === 'my-string';
+        }
+
+        @handle()
+        MyOtherCommand(command: MyOtherCommand): boolean {
+          return command.key === 'my-string';
+        }
+      }
+      const controller = new MyController();
+      controller.registerCommandHandler = sinon.stub();
+      container.injectInto(controller);
+
+      expect(controller.registerCommandHandler).to.be.calledTwice;
+      expect(controller.registerCommandHandler).to.be.calledWithExactly(
+        MyCommand,
+        controller.MyCommand
+      );
+      expect(controller.registerCommandHandler).to.be.calledWithExactly(
+        MyOtherCommand,
+        controller.MyOtherCommand
+      );
+    });
+
+    it('overrides initialize method from OneToOneHandlingMixin thus not initializing event subscriptions mappings', () => {
+      class MyController extends CommandHandlingMixin {
+        @subscribe()
+        MyEvent(event: MyEvent): boolean {
+          return event.key === 'my-string';
+        }
+      }
+      const controller = new MyController();
+      controller.registerHandler = sinon.stub();
+      controller.initialize();
+      expect(controller.registerHandler).to.not.be.called;
+    });
+
+    it(`throws UnhandleableTypeError upon types not subclassing from Command defined as handlers`, () => {
+      class MyController extends CommandHandlingMixin {
+        handles(): Map<types.MessageableType, types.Handler> {
+          return new Map([[MyEvent, sinon.stub()]]);
+        }
+      }
+      const controller = new MyController();
+      expect(() => controller.initialize()).to.throw(
+        UnhandleableTypeError,
+        `MyController: type must be one of: [Command]; got MyEvent`
+      );
+    });
+  });
+
+  describe('registration', () => {
+    it('throws UnhandleableTypeError upon registering non command type', () => {
+      class MyController extends CommandHandlingMixin {}
+      const controller = new MyController();
+      expect(() => {
+        controller.registerCommandHandler(MyEvent, sinon.stub());
+      }).to.throw(
+        UnhandleableTypeError,
+        'MyController: type must be one of: [Command]; got MyEvent'
+      );
+    });
+
+    it(`registers command handler`, () => {
+      const handler = sinon.spy();
+      class MyController extends CommandHandlingMixin {}
+      const controller = new MyController();
+      container.injectInto(controller);
+      controller.registerHandler = sinon.stub();
+
+      controller.registerCommandHandler(MyCommand, handler);
+      expect(controller.registerHandler).to.be.calledOnce;
+    });
+
+    it(`registers handler on instance with bound handler`, () => {
+      const handler = sinon.stub();
+      class MyController extends CommandHandlingMixin {}
+      const controller = new MyController();
+      const registerHandler = sinon.stub(controller, 'registerHandler');
+      container.injectInto(controller);
+
+      controller.registerCommandHandler(MyCommand, handler);
+      expect(registerHandler).to.be.calledOnce;
+      expect(registerHandler.args[0][0]).to.be.equal(MyCommand);
+      expect(
+        Object.create(handler.prototype) instanceof registerHandler.args[0][1]
+      ).to.be.true; // Compare bound function to handler function example
+      expect(registerHandler.args[0][2]).to.be.false; // Flag shouldOverride set by default to false
+    });
+
+    it(`registers handler on CommandBus with bound handler`, () => {
+      const handler = sinon.stub();
+      class MyController extends CommandHandlingMixin {}
+      const controller = new MyController();
+      container.injectInto(controller);
+
+      controller.registerCommandHandler(MyCommand, handler);
+      expect(commandBus.registerHandler).to.be.calledOnce;
+      expect(commandBus.registerHandler.args[0][0]).to.be.equal(MyCommand);
+      expect(
+        Object.create(handler.prototype) instanceof
+          commandBus.registerHandler.args[0][1]
+      ).to.be.true; // Compare bound function to handler function example
+      expect(commandBus.registerHandler.args[0][2]).to.be.false; // Flag shouldOverride set by default to false
+    });
+
+    it(`ensures that context of registered handler on CommandBus is bound to instance`, () => {
+      commandBus.handlers = new Map();
+      commandBus.registerHandler = function(command, handler): void {
+        this.handlers.set(command, handler);
+      };
+
+      class MyController extends CommandHandlingMixin {
+        dependency: any;
+
+        MyCommand(command: MyCommand): void {
+          return this.dependency(command);
+        }
+      }
+      const controller = new MyController();
+      controller.dependency = sinon.stub();
+      controller.commandBus = commandBus;
+
+      controller.registerCommandHandler(MyCommand, controller.MyCommand);
+      const commandInstance = new MyCommand({
+        targetId: 'my-target-id',
+        key: 'my-key',
+      });
+      commandBus.handlers.get(MyCommand)(commandInstance);
+      expect(controller.dependency).to.be.calledOnce;
+      expect(controller.dependency).to.be.calledWithExactly(commandInstance);
+    });
+  });
+
+  describe('command handling', () => {
+    it(`allows to send command through command bus`, async () => {
+      class MyController extends CommandHandlingMixin {}
+      const controller = new MyController();
+      container.injectInto(controller);
+
+      const commandInstance = new MyCommand({
+        targetId: 'my-target-id',
+        key: 'my-key',
+      });
+      await controller.send(commandInstance);
+      expect(commandBus.send).to.be.calledOnce;
+      expect(commandBus.send).to.be.calledWithExactly(commandInstance);
+    });
+
+    it(`ensures that result is passed back from handler upon sending command`, async () => {
+      commandBus.send.returns('result');
+
+      class MyController extends CommandHandlingMixin {}
+      const controller = new MyController();
+      container.injectInto(controller);
+
+      const commandInstance = new MyCommand({
+        targetId: 'my-target-id',
+        key: 'my-key',
+      });
+      const result = await controller.send(commandInstance);
+      expect(result).to.be.equal('result');
+    });
+  });
+});
