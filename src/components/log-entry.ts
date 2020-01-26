2e1fb1f144c94f76274694e43c60c125de771826
Adam Desivi <desivi@desivi.com>
2020-01-26 17:13:47 +0100

    feat(components): add LogEntry

---
+++ src/components/log-entry.ts
@@ -0,0 +1,181 @@
+import { isString } from 'lodash';
+import { getTypeName } from '@eveble/helpers';
+import { types } from '../types';
+import { ExtendableError } from './extendable-error';
+
+export class UndefinedLoggableTargetError extends ExtendableError {
+  constructor() {
+    super(
+      `Please define your logged target with 'Log.prototype.on' method at your Log instance`
+    );
+  }
+}
+
+export class LogMetadata implements types.LogMetadata {
+  public readonly description: string;
+
+  public readonly value?: any;
+
+  public readonly keys?: string[];
+
+  /**
+   * Creates an instance LogMetadata.
+   * @param description - Description of logged metadata entry.
+   * @param value - Logged value.
+   * @param keys - Optional array of properties keys when provided value is an object. Allows to display only selected ones back on log entry.
+   */
+  constructor(description: string, value?: any, keys?: string[]) {
+    this.description = description;
+    if (value) this.value = value;
+    if (keys) this.keys = keys;
+  }
+}
+
+export class Log implements types.LogEntry, types.Stringifiable {
+  public readonly message: string;
+
+  public readonly metadata: Map<string, types.LogMetadata>;
+
+  public readonly options: types.LogFormatting;
+
+  public level: types.LogLevel;
+
+  private target?: any;
+
+  public method?: () => types.AnyFunction;
+
+  public methodName?: string;
+
+  public typeName?: types.TypeName | undefined;
+
+  /**
+   * Creates an instance Log.
+   * @param messageOrProps - Message of a log as a `string` or log properties.
+   */
+  constructor(messageOrProps: string | Record<keyof any, any>) {
+    if (typeof messageOrProps === 'string') {
+      const message = messageOrProps;
+      this.message = message;
+      this.metadata = new Map();
+      this.options = {};
+      this.method = undefined;
+      this.methodName = '';
+      this.typeName = '';
+    } else {
+      const props = messageOrProps;
+      Object.assign(this, props);
+    }
+  }
+
+  /**
+   * Converts log to string representation.
+   * @returns Log message as a `string`.
+   */
+  public toString(): string {
+    return this.message;
+  }
+
+  /**
+   * Sets target of the log.
+   * @param target - The target on which log is logged(class).
+   * @returns Instance of self.
+   */
+  public on(target: any): this {
+    this.target = target;
+    this.typeName = getTypeName(this.target) || '';
+    return this;
+  }
+
+  /**
+   * Sets method in which log is invoked or it's name.
+   * @param methodOrName - Method in which log is invoked or its name.
+   * @returns Instance of self.
+   */
+  public in(methodOrName: types.AnyFunction | string): this {
+    if (this.target === undefined) {
+      throw new UndefinedLoggableTargetError();
+    }
+    let method: types.AnyFunction;
+    if (isString(methodOrName)) {
+      this.methodName = methodOrName;
+      method = this.target[this.methodName];
+    } else {
+      method = methodOrName;
+      this.methodName = method.name;
+    }
+    this.method = method;
+
+    return this;
+  }
+
+  /**
+   * Adds additional metadata about log entry.
+   * @param description - Description of logged metadata entry.
+   * @param value - Optional logged value.
+   * @param keys - Optional array of properties keys when provided value is an object. Allows to display only selected ones back on log entry.
+   * @returns Instance of self.
+   */
+  public with(description: string, value?: any, keys?: string[]): this {
+    const metadata = new LogMetadata(description, value, keys);
+    this.metadata.set(description, metadata);
+
+    return this;
+  }
+
+  /**
+   * Sets additional formatting options for log entry.
+   * @param options - Log formatting options as an object.
+   * @returns Instance of self.
+   */
+  public format(options: types.LogFormatting): this {
+    Object.assign(this.options, options);
+    return this;
+  }
+
+  /**
+   * Returns target.
+   * @return Class instance.
+   */
+  public getTarget(): any {
+    return this.target;
+  }
+
+  /**
+   * Evaluates if defined method is a static method.
+   * @returns Returns `true` if is static method, else `false`.
+   */
+  public isStaticMethod(): boolean {
+    if (this.target !== undefined && this.method !== undefined) {
+      return (
+        this.target.constructor !== undefined &&
+        this.target.constructor[this.method.name] !== undefined
+      );
+    }
+    return false;
+  }
+
+  /**
+   * Returns metadata for log entry.
+   * @returns `LogMetadata` instance if present.
+   */
+  public getMetadata(description: string): types.LogMetadata | undefined {
+    return this.metadata.get(description);
+  }
+
+  /**
+   * Evaluates if log entry has metadata with description set on collection.
+   * @returns Returns `true` if log entry has metadata, else `false`.
+   */
+  public hasMetadata(description: string): boolean {
+    return this.metadata.has(description);
+  }
+
+  /**
+   * Sets logging level for which log entry is logged.
+   * @param level - Logged level.
+   */
+  public setLevel(level: types.LogLevel): this {
+    this.level = level;
+    return this;
+  }
+}
---
+++ test/unit/components/log-entry.test.ts
@@ -0,0 +1,206 @@
+import { expect } from 'chai';
+import { Log, LogMetadata } from '../../../src/components/log-entry';
+import { define } from '../../../src/decorators/define';
+import { DefinableMixin } from '../../../src/mixins/definable-mixin';
+
+describe('Log', function() {
+  @define('MyClass')
+  class MyClass extends DefinableMixin {
+    myMethod(first: string, second: string, third: string): void {
+      Object.assign(this, { first, second, third });
+    }
+
+    static myStaticMethod(first: string, second: string): void {
+      Object.assign(this, { first, second });
+    }
+  }
+
+  const message = 'my-message';
+
+  describe('construction', () => {
+    it('takes message as a string and assigns it', () => {
+      const log = new Log(message);
+      expect(log.message).to.be.equal(message);
+    });
+
+    it('initializes with empty metadata map', () => {
+      const log = new Log(message);
+      expect(log.metadata).to.be.instanceOf(Map);
+      expect(log.metadata).to.be.empty;
+    });
+
+    it('initializes with empty options object', () => {
+      const log = new Log(message);
+      expect(log.options).to.be.instanceOf(Object);
+      expect(log.options).to.be.empty;
+    });
+  });
+
+  describe('conversion', () => {
+    it('converts log message to string', () => {
+      expect(new Log(message).toString()).to.be.equal(message);
+    });
+  });
+
+  describe('describing log', () => {
+    context('on', () => {
+      it(`sets target 'on' which log was done`, () => {
+        const target = new MyClass();
+        const log = new Log(message).on(target);
+        expect(log.getTarget()).to.be.equal(target);
+      });
+    });
+
+    context('in', () => {
+      it(`sets target's method function 'in' which log was done`, () => {
+        const target = new MyClass();
+        const log = new Log(message).on(target).in(target.myMethod);
+        expect(log.getTarget()).to.be.equal(target);
+        expect(log.method).to.be.equal(target.myMethod);
+        expect(log.methodName).to.be.equal('myMethod');
+      });
+
+      it(`sets target's method function name as a String 'in' which log was done`, () => {
+        const target = new MyClass();
+        const log = new Log(message).on(target).in('myMethod');
+        expect(log.getTarget()).to.be.equal(target);
+        expect(log.methodName).to.be.equal('myMethod');
+        expect(log.method).to.be.equal(target.myMethod);
+      });
+    });
+
+    context('with', () => {
+      it(`adds additional metadata 'with' is related to log`, () => {
+        const result = {
+          first: 'first',
+        };
+        const keys = ['first', 'second'];
+        const target = new MyClass();
+        const log = new Log(message)
+          .on(target)
+          .in(target.myMethod)
+          .with('result', result, keys);
+        expect(log.hasMetadata('result')).to.be.true;
+        expect(log.getMetadata('result')).to.be.instanceof(LogMetadata);
+        expect(log.getMetadata('result')).to.be.eql(
+          new LogMetadata('result', result, keys)
+        );
+      });
+
+      it(`allows to add multiple metadata items`, () => {
+        const target = new MyClass();
+        const log = new Log(message)
+          .on(target)
+          .in(target.myMethod)
+          .with('first', 'first-value')
+          .with('second', 'second-value');
+
+        expect(log.hasMetadata('first')).to.be.true;
+        expect(log.getMetadata('first')).to.be.instanceof(LogMetadata);
+        expect(log.getMetadata('first')).to.be.eql(
+          new LogMetadata('first', 'first-value')
+        );
+
+        expect(log.hasMetadata('second')).to.be.true;
+        expect(log.getMetadata('second')).to.be.instanceof(LogMetadata);
+        expect(log.getMetadata('second')).to.be.eql(
+          new LogMetadata('second', 'second-value')
+        );
+      });
+
+      it(`allows to add value-less metadata that converter handles explicitly`, () => {
+        const target = new MyClass();
+        const log = new Log(message)
+          .on(target)
+          .in(target.myMethod)
+          .with('properties');
+        expect(log.hasMetadata('properties')).to.be.true;
+        expect(log.getMetadata('properties')).to.be.instanceof(LogMetadata);
+        expect(log.getMetadata('properties')).to.be.eql(
+          new LogMetadata('properties')
+        );
+      });
+    });
+
+    context('format', () => {
+      it('allows to define additional formatting options', () => {
+        const options = { isSimple: true };
+        const target = new MyClass();
+        const log = new Log(message).on(target).format(options);
+
+        expect(log.options).to.be.eql(options);
+      });
+
+      it('ensures that formatting options can be chained', () => {
+        const firstOptions = { isSimple: true };
+        const secondOptions = { isColored: false };
+        const target = new MyClass();
+        const log = new Log(message)
+          .on(target)
+          .format(firstOptions)
+          .format(secondOptions);
+
+        expect(log.options).to.be.eql({ ...firstOptions, ...secondOptions });
+      });
+    });
+
+    context('level', () => {
+      it('allows to set log entry level', () => {
+        const log = new Log(message).setLevel('debug');
+        expect(log.level).to.be.equal('debug');
+      });
+    });
+  });
+
+  describe('getters', () => {
+    describe('type name', () => {
+      it(`returns target type name as type name`, () => {
+        const target = new MyClass();
+        const log = new Log(message).on(target);
+        expect(log.typeName).to.be.equal('MyClass');
+      });
+
+      it(`returns target name based on constructor name`, () => {
+        class MyOtherClass {}
+
+        const target = new MyOtherClass();
+        const log = new Log(message).on(target);
+        expect(log.typeName).to.be.equal('MyOtherClass');
+      });
+
+      it(`returns empty string if target is not set`, () => {
+        const log = new Log(message);
+        expect(log.typeName).to.be.equal('');
+      });
+    });
+
+    describe('method name', () => {
+      it(`returns method name based on function's name property`, () => {
+        const target = new MyClass();
+        const log = new Log(message).on(target).in('myMethod');
+        expect(log.methodName).to.be.equal('myMethod');
+      });
+      it(`returns method name based on direct named function`, () => {
+        const target = new MyClass();
+        const log = new Log(message).on(target).in(target.myMethod);
+        expect(log.methodName).to.be.equal('myMethod');
+      });
+    });
+  });
+
+  describe('evaluation', () => {
+    describe('isStaticMethod', () => {
+      it('returns true if assigned method is static', () => {
+        const target = new MyClass();
+        const log = new Log(message).on(target).in(MyClass.myStaticMethod);
+        expect(log.methodName).to.be.equal('myStaticMethod');
+        expect(log.isStaticMethod()).to.be.true;
+      });
+      it('returns false if assigned method is prototype(instance)', () => {
+        const target = new MyClass();
+        const log = new Log(message).on(target).in(target.myMethod);
+        expect(log.isStaticMethod()).to.be.false;
+      });
+    });
+  });
+});
