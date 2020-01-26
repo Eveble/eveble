746f487c28e4046044430ce97e7c661494f3d299
Adam Desivi <desivi@desivi.com>
2020-01-26 18:06:10 +0100

    feat(mixins): add mixin for registering single handler for individual message type

---
+++ src/mixins/one-to-one-handling-mixin.ts
@@ -0,0 +1,175 @@
+import { getTypeName } from '@eveble/helpers';
+import { isFunction } from 'lodash';
+import { instanceOf } from 'typend';
+import { postConstruct } from '@parisholley/inversify-async';
+import { HandlingMixin } from './handling-mixin';
+import { kernel } from '../core/kernel';
+import {
+  UnhandleableTypeError,
+  InvalidHandlerError,
+  HandlerExistError,
+  HandlerNotFoundError,
+  InvalidMessageableType,
+} from '../messaging/messaging-errors';
+import { types } from '../types';
+import { Command } from '../components/command';
+import { Event } from '../components/event';
+
+export class OneToOneHandlingMixin extends HandlingMixin
+  implements types.Controller {
+  protected handlers: Map<types.MessageableType, types.Handler>;
+
+  /**
+   * Initializes OneToOneHandlingMixin.
+   * @remarks
+   * By defining handler mappings on metadata with annotations: `@handle`,
+   * `@subscribe` and resolving those mapping with `handles`, `subscribes` method -
+   * it ensures, that there is no leakage in between inheriting classes.
+   */
+  @postConstruct()
+  public initialize(): void {
+    this.setupHandlers({
+      handlers: this.handles(),
+      handleableTypes: [Command],
+    });
+    this.setupHandlers({
+      handlers: this.subscribes(),
+      handleableTypes: [Event],
+    });
+  }
+
+  /**
+   * Registers handler for message type.
+   * @param messageType - Type implementing `MessageableType` interface.
+   * @param handler - Handler function that will executed upon handling message type.
+   * @param shouldOverride - Flag indicating that handler should be overridden if exist.
+   * @throws {UnhandleableTypeError}
+   * Thrown if the type argument is not one of handleable types.
+   * @throws {InvalidHandlerError}
+   * Thrown if the handler argument is not a function.
+   * @throws {HandlerExistError}
+   * Thrown if handler would overridden without explicit call.
+   * @example
+   *```ts
+   * @define('MyCommand')
+   * class MyCommand extends Command {
+   *   key: string;
+   * }
+   * define('MyOtherCommand')
+   * class MyOtherCommand extends Command {
+   *   key: string;
+   * }
+   *
+   * class MyClass extends OneToOneHandlingMixin {
+   *   MyCommand(command: MyCommand): void {
+   *     // ...
+   *   }
+   *
+   *   @handles()
+   *   MyOtherCommand(command: MyOtherCommand): void {
+   *     // ...
+   *   }
+   * }
+   * const myClass = new MyClass();
+   * // Defined externally or during construction
+   * myClass.registerHandler(MyCommand, myClass.MyCommand);
+   * ```
+   */
+  public registerHandler(
+    messageType: types.MessageableType,
+    handler: types.Handler,
+    shouldOverride = false
+  ): void {
+    if (!this.isHandleabe(messageType)) {
+      throw new UnhandleableTypeError(
+        getTypeName(this.constructor) as types.TypeName,
+        kernel.describer.describe(this.getHandleableTypes()),
+        kernel.describer.describe(messageType)
+      );
+    }
+
+    if (!isFunction(handler)) {
+      throw new InvalidHandlerError(
+        getTypeName(this.constructor) as types.TypeName,
+        messageType.getTypeName(),
+        kernel.describer.describe(handler)
+      );
+    }
+
+    if (this.hasHandler(messageType) && !shouldOverride) {
+      throw new HandlerExistError(
+        getTypeName(this.constructor) as types.TypeName,
+        messageType.getTypeName()
+      );
+    }
+
+    this.handlers.set(messageType, handler);
+  }
+
+  /**
+   * Returns handler for message type.
+   * @param messageType - Type implementing `MessageableType` interface.
+   * @returns Handler as a function if found, else `undefined`.
+   * @throws {InvalidMessageableType}
+   * Thrown if the message type argument is not implementing `Messageable` interface.
+   */
+  public getHandler(
+    messageType: types.MessageableType
+  ): types.Handler | undefined {
+    if (!instanceOf<types.Messageable>(messageType.prototype)) {
+      throw new InvalidMessageableType(kernel.describer.describe(messageType));
+    }
+    return this.hasHandler(messageType)
+      ? this.handlers.get(messageType)
+      : undefined;
+  }
+
+  /**
+   * Return handler for message type or throws error if not found.
+   * @param messageType - Type implementing `MessageableType` interface.
+   * @returns Handler as a function if found, else throws.
+   * @throws {HandlerNotFoundError}
+   * Thrown if handler for message type is not found.
+   */
+  public getHandlerOrThrow(messageType: types.MessageableType): types.Handler {
+    const handler = this.getHandler(messageType);
+
+    if (handler === undefined) {
+      throw new HandlerNotFoundError(
+        getTypeName(this.constructor) as types.TypeName,
+        messageType.getTypeName()
+      );
+    }
+    return handler;
+  }
+
+  /**
+   * Resolves message type by handler reference.
+   * @param handlerReference - Reference to handler function.
+   * @returns Message type if handler is matching one of handlers on class, else `undefined`.
+   */
+  public getTypeByHandler(handlerReference: types.Handler): any | undefined {
+    for (const [messageType, handler] of this.handlers.entries()) {
+      const unboundHandler = (handler as any).original;
+      // Compare original function and bound ones as a fallback
+      if (handlerReference === unboundHandler || handlerReference === handler) {
+        return messageType;
+      }
+    }
+    return undefined;
+  }
+
+  /**
+   * Handles message type instance.
+   * @async
+   * @param message - Type implementing `Messageable` interface.
+   * @returns Any value returned by successful or unsuccessful handling.
+   * @throws {HandlerNotFoundError}
+   * Thrown if handler for type is not found.
+   */
+  public async handle(message: types.Messageable): Promise<any> {
+    const handler = this.getHandlerOrThrow(message.constructor);
+    const result = await handler(message);
+    return result;
+  }
+}
---
+++ test/unit/mixins/one-to-one-handling-mixin.test.ts
@@ -0,0 +1,370 @@
+import chai, { expect } from 'chai';
+import sinon from 'sinon';
+import sinonChai from 'sinon-chai';
+import chaiAsPromised from 'chai-as-promised';
+import { Command } from '../../../src/components/command';
+import { Event } from '../../../src/components/event';
+import { types } from '../../../src/types';
+import { OneToOneHandlingMixin } from '../../../src/mixins/one-to-one-handling-mixin';
+import { handle } from '../../../src/annotations/handle';
+import { Message } from '../../../src/components/message';
+import {
+  UnhandleableTypeError,
+  HandlerNotFoundError,
+  HandlerExistError,
+  InvalidHandlerError,
+  InvalidMessageableType,
+} from '../../../src/messaging/messaging-errors';
+import { define } from '../../../src/decorators/define';
+import { hasPostConstruct } from '../../../src/utils/helpers';
+import { subscribe } from '../../../src/annotations/subscribe';
+import { HandlingMixin } from '../../../src/mixins/handling-mixin';
+
+chai.use(chaiAsPromised);
+chai.use(sinonChai);
+
+describe('OneToOneHandlingMixin', function() {
+  @define('MyCommand', { isRegistrable: false })
+  class MyCommand extends Command {
+    key: string;
+  }
+
+  @define('Namespaced.Command', { isRegistrable: false })
+  class NamespacedCommand extends Command {
+    key: string;
+  }
+
+  @define('MyEvent', { isRegistrable: false })
+  class MyEvent extends Event {
+    key: string;
+  }
+
+  it('extends HandlingMixin', () => {
+    expect(OneToOneHandlingMixin.prototype).to.be.instanceof(HandlingMixin);
+  });
+
+  describe('construction', () => {
+    it('ensures that controller can be initialized without handlers', () => {
+      class MyController extends OneToOneHandlingMixin {}
+      expect(() => new MyController()).to.not.throw(Error);
+    });
+
+    it('initializes with empty handlers as instance of map', () => {
+      class MyController extends OneToOneHandlingMixin {}
+      const controller = new MyController();
+      expect(controller.getHandlers()).to.be.instanceof(Map);
+      expect(controller.getHandlers()).to.be.empty;
+    });
+
+    it('initializes with empty handleable types as instance of array', () => {
+      class MyController extends OneToOneHandlingMixin {}
+      const controller = new MyController();
+      expect(controller.getHandleableTypes()).to.be.instanceof(Array);
+      expect(controller.getHandleableTypes()).to.be.eql([Message]);
+    });
+  });
+
+  describe('initialization', () => {
+    it('setups all handlers from handlers mapping method on initialization', () => {
+      class MyController extends OneToOneHandlingMixin {
+        @handle()
+        MyCommandHandlerMethod(command: MyCommand): boolean {
+          return command.key === 'my-string';
+        }
+      }
+      const controller = new MyController();
+      controller.initialize();
+      expect(controller.getHandlers()).to.be.instanceof(Map);
+      expect(controller.getHandlers()).to.be.eql(
+        new Map([[MyCommand, controller.MyCommandHandlerMethod]])
+      );
+    });
+
+    it('setups all handlers from subscribes mapping method on initialization', () => {
+      class MyController extends OneToOneHandlingMixin {
+        @subscribe()
+        MyEventHandlerMethod(event: MyEvent): boolean {
+          return event.key === 'my-string';
+        }
+      }
+      const controller = new MyController();
+      controller.initialize();
+      expect(controller.getHandlers()).to.be.instanceof(Map);
+      expect(controller.getHandlers()).to.be.eql(
+        new Map([[MyEvent, controller.MyEventHandlerMethod]])
+      );
+    });
+
+    it('annotates initializes as post construction method for Inversify', () => {
+      class MyController extends OneToOneHandlingMixin {}
+      const controller = new MyController();
+      expect(hasPostConstruct(controller)).to.be.true;
+    });
+  });
+
+  describe('handler registration', () => {
+    it('throws UnhandleableTypeError if provided message type is not handleabe', () => {
+      class MyController extends OneToOneHandlingMixin {}
+      const controller = new MyController();
+
+      class InvalidType {}
+      expect(() =>
+        controller.registerHandler(InvalidType, sinon.stub())
+      ).to.throw(
+        UnhandleableTypeError,
+        'MyController: type must be one of: [Message]; got InvalidType'
+      );
+    });
+
+    it('throws InvalidHandlerError if provided handler is not a function', () => {
+      class MyController extends OneToOneHandlingMixin {}
+      const controller = new MyController();
+
+      expect(() =>
+        controller.registerHandler(
+          MyCommand,
+          (undefined as any) as types.Handler
+        )
+      ).to.throw(
+        InvalidHandlerError,
+        `MyController: provided handler for 'MyCommand' must be a function, got undefined`
+      );
+    });
+
+    it('throws HandlerExistsError if handler for a message type is already registered', () => {
+      class MyController extends OneToOneHandlingMixin {}
+      const controller = new MyController();
+
+      controller.registerHandler(MyCommand, sinon.stub());
+      expect(() =>
+        controller.registerHandler(MyCommand, sinon.stub())
+      ).to.throw(
+        HandlerExistError,
+        `MyController: handler for 'MyCommand' already exists`
+      );
+    });
+
+    it('registers one to one relational handler for a message type', () => {
+      class MyController extends OneToOneHandlingMixin {}
+      const controller = new MyController();
+
+      const handler = sinon.stub();
+      controller.registerHandler(MyCommand, handler);
+      expect(controller.getHandler(MyCommand)).to.be.equal(handler);
+    });
+
+    it('registers one to one relational handler for a namespaced type', () => {
+      class MyController extends OneToOneHandlingMixin {}
+      const controller = new MyController();
+
+      const handler = sinon.stub();
+      controller.registerHandler(NamespacedCommand, handler);
+      expect(controller.getHandler(NamespacedCommand)).to.be.equal(handler);
+    });
+  });
+
+  describe('override', () => {
+    it('allows to override already registered one to one handler for a message type', () => {
+      class MyController extends OneToOneHandlingMixin {}
+      const controller = new MyController();
+
+      const handler = sinon.stub();
+      const otherHandler = sinon.stub();
+      controller.registerHandler(MyCommand, handler);
+      expect(() =>
+        controller.overrideHandler(MyCommand, otherHandler)
+      ).to.not.throw(HandlerExistError);
+      expect(controller.getHandler(MyCommand)).to.be.equal(otherHandler);
+    });
+  });
+
+  describe('evaluating handlers', () => {
+    it('returns true if message type has a registered handler', () => {
+      class MyController extends OneToOneHandlingMixin {}
+      const controller = new MyController();
+
+      controller.registerHandler(NamespacedCommand, sinon.stub());
+      expect(controller.hasHandler(NamespacedCommand)).to.be.true;
+    });
+
+    it('returns false if message type has no registered handler', () => {
+      class MyController extends OneToOneHandlingMixin {}
+      const controller = new MyController();
+      expect(controller.hasHandler(NamespacedCommand)).to.be.false;
+    });
+  });
+
+  describe('resolving handler', () => {
+    describe('getHandler', () => {
+      it('throws InvalidMessageableType if provided value is not implementing Messageable interface', () => {
+        class MyController extends OneToOneHandlingMixin {}
+        const controller = new MyController();
+
+        class InvalidType {}
+        expect(() => controller.getHandler(InvalidType)).to.throw(
+          InvalidMessageableType,
+          `Type 'InvalidType' must implement Messageable interface`
+        );
+      });
+
+      it('returns handler for message type as a function', () => {
+        class MyController extends OneToOneHandlingMixin {}
+        const controller = new MyController();
+
+        const handler = sinon.stub();
+        controller.registerHandler(MyCommand, handler);
+        expect(controller.getHandler(MyCommand)).to.be.equal(handler);
+      });
+
+      it('returns handler for namespaced message type as a function', () => {
+        class MyController extends OneToOneHandlingMixin {}
+        const controller = new MyController();
+
+        const handler = sinon.stub();
+        controller.registerHandler(NamespacedCommand, handler);
+        expect(controller.getHandler(NamespacedCommand)).to.be.equal(handler);
+      });
+
+      it('returns undefined for message type that does not have registered handler', () => {
+        class MyController extends OneToOneHandlingMixin {}
+        const controller = new MyController();
+
+        expect(controller.getHandler(MyCommand)).to.be.equal(undefined);
+      });
+    });
+
+    describe('getHandlerOrThrow', () => {
+      it('throws InvalidMessageableType if provided value is not implementing Messageable interface', () => {
+        class MyController extends OneToOneHandlingMixin {}
+        const controller = new MyController();
+
+        class InvalidType {}
+        expect(() => controller.getHandlerOrThrow(InvalidType)).to.throw(
+          InvalidMessageableType,
+          `Type 'InvalidType' must implement Messageable interface`
+        );
+      });
+
+      it('throws HandlerNotFoundError if there is no handler registered for provided message type', () => {
+        class MyController extends OneToOneHandlingMixin {}
+        const controller = new MyController();
+
+        expect(() => controller.getHandlerOrThrow(MyCommand)).to.throw(
+          HandlerNotFoundError,
+          `MyController: handler for type 'MyCommand' can't be found`
+        );
+      });
+
+      it('returns handler for message type as a function', () => {
+        class MyController extends OneToOneHandlingMixin {}
+        const controller = new MyController();
+
+        const handler = sinon.stub();
+        controller.registerHandler(MyCommand, handler);
+        expect(controller.getHandlerOrThrow(MyCommand)).to.be.equal(handler);
+      });
+
+      it('returns handler for namespaced message type as a function', () => {
+        class MyController extends OneToOneHandlingMixin {}
+        const controller = new MyController();
+
+        const handler = sinon.stub();
+        controller.registerHandler(NamespacedCommand, handler);
+        expect(controller.getHandlerOrThrow(NamespacedCommand)).to.be.equal(
+          handler
+        );
+      });
+    });
+
+    describe('getTypeByHandler', () => {
+      it('resolves message type by handler reference', () => {
+        class MyController extends OneToOneHandlingMixin {}
+        const controller = new MyController();
+
+        const handler = sinon.stub();
+        controller.registerHandler(MyEvent, sinon.stub());
+        controller.registerHandler(MyCommand, handler);
+        expect(controller.getTypeByHandler(handler)).to.be.equal(MyCommand);
+      });
+
+      it('resolves message type for bound handler reference', () => {
+        class MyController extends OneToOneHandlingMixin {
+          registerCommandHandler(
+            command: any,
+            fn: Function,
+            shouldOverride = false
+          ): void {
+            const handler = fn.bind(this);
+            handler.original = fn;
+            this.registerHandler(command, handler, shouldOverride);
+          }
+        }
+        const controller = new MyController();
+
+        const handler = sinon.stub();
+        controller.registerCommandHandler(NamespacedCommand, sinon.stub());
+        controller.registerCommandHandler(MyCommand, handler);
+        expect(controller.getTypeByHandler(handler)).to.be.equal(MyCommand);
+      });
+
+      it('returns undefined for unregistered handler reference', () => {
+        class MyController extends OneToOneHandlingMixin {}
+        const controller = new MyController();
+
+        const handler = sinon.stub();
+        expect(controller.getTypeByHandler(handler)).to.be.undefined;
+      });
+    });
+  });
+
+  describe('manipulation', () => {
+    it('removes handler for message type', () => {
+      class MyController extends OneToOneHandlingMixin {}
+      const controller = new MyController();
+
+      controller.registerHandler(MyCommand, sinon.stub());
+      expect(controller.hasHandler(MyCommand)).to.be.true;
+      controller.removeHandler(MyCommand);
+      expect(controller.hasHandler(MyCommand)).to.be.false;
+    });
+  });
+
+  describe('handling', () => {
+    it(`throws HandlerNotFoundError if handler for message type can't be found`, async () => {
+      class MyController extends OneToOneHandlingMixin {}
+      const controller = new MyController();
+
+      const command = new MyCommand({ targetId: 'my-id', key: 'my-string' });
+      expect(controller.handle(command)).to.eventually.be.rejectedWith(
+        HandlerNotFoundError,
+        `MyController: handler for type 'MyCommand' can't be found`
+      );
+    });
+
+    it('handles message type instance with one to one relational handler', async () => {
+      class MyController extends OneToOneHandlingMixin {}
+      const controller = new MyController();
+
+      const handler = sinon.stub();
+      controller.registerHandler(MyCommand, handler);
+
+      const command = new MyCommand({ targetId: 'my-id', key: 'my-string' });
+      await controller.handle(command);
+      expect(handler).to.be.calledOnce;
+      expect(handler).to.be.calledWithExactly(command);
+    });
+
+    it('ensures that result of handling one to one relational handler is returned back', async () => {
+      class MyController extends OneToOneHandlingMixin {}
+      const controller = new MyController();
+
+      const handler = sinon.stub();
+      const result = 'my-result';
+      handler.returns(result);
+      controller.registerHandler(MyCommand, handler);
+
+      const command = new MyCommand({ targetId: 'my-id', key: 'my-string' });
+      expect(await controller.handle(command)).to.be.equal(result);
+    });
+  });
+});
