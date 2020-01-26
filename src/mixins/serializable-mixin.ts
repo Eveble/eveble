9e58ca1963d85a6ba4d804f2a71d219057225e17
Adam Desivi <desivi@desivi.com>
2020-01-26 17:27:26 +0100

    feat(mixins): add SerializableMixin

---
+++ src/mixins/serializable-mixin.ts
@@ -0,0 +1,81 @@
+import { getTypeName } from '@eveble/helpers';
+import { types } from '../types';
+import { kernel } from '../core/kernel';
+
+export class SerializableMixin {
+  /**
+   * Returns definable type name.
+   * @returns Type name as a string.
+   */
+  public getTypeName(): types.TypeName {
+    return getTypeName(this) as types.TypeName;
+  }
+
+  /**
+   * Returns definable type name
+   * @returns Type name as a string.
+   */
+  public toString(): types.TypeName {
+    return this.getTypeName();
+  }
+
+  /**
+   * Returns definable type name
+   * @returns Type name as a string.
+   */
+  public static toString(): types.TypeName {
+    return this.getTypeName();
+  }
+
+  /**
+   * Returns definable type name.
+   * @returns Type name as a string.
+   */
+  public static getTypeName(): types.TypeName {
+    return getTypeName(this) as types.TypeName;
+  }
+
+  /**
+   * Serializes value into a JSON-compatible value. It preserves all custom
+   * field types, however the initial value type is not saved.
+   * @returns Normalized value as JSON-compatible without type identifers.
+   * @example
+   *```ts
+   * @define('Address')
+   * class Address extends Serializable {
+   *   city: string;
+   *
+   *   street: string;
+   * }
+   *
+   * @define('Person')
+   * class Person extends Serializable {
+   *   firstName: string;
+   *
+   *   lastName: string;
+   *
+   *   address?: Address;
+   * }
+   *
+   * const person = new Person({
+   *   firstName: 'Jane',
+   *   lastName: 'Doe',
+   *   address: new Address({
+   *     city: 'New York',
+   *     street: 'Wall Street',
+   *   }),
+   * });
+   *
+   * expect(person.toJSONValue()).to.be.eql({
+   *   firstName: 'Jane',
+   *   lastName: 'Doe',
+   *   address: {
+   *     city: 'New York',
+   *     street: 'Wall Street',
+   *   },
+   * });
+   */
+  public toJSONValue(): Record<string, any> {
+    return kernel.serializer.toJSONValue(this);
+  }
+}
---
+++ test/unit/mixins/serializabe-mixin.test.ts
@@ -0,0 +1,78 @@
+import chai, { expect } from 'chai';
+import { SerializableMixin } from '../../../src/mixins/serializable-mixin';
+import { kernel } from '../../../src/core/kernel';
+import { stubInterface } from 'ts-sinon';
+import { types } from '../../../src/types';
+import { define } from '../../../src/decorators/define';
+import sinon from 'sinon';
+import sinonChai from 'sinon-chai';
+
+chai.use(sinonChai);
+
+describe('SerializableMixin', () => {
+  let originalConverter: any;
+  let converter: any;
+  let serializer: any;
+
+  before(() => {
+    originalConverter = kernel.converter;
+  });
+
+  beforeEach(() => {
+    converter = stubInterface<types.Converter>();
+    kernel.setConverter(converter);
+
+    serializer = stubInterface<types.Serializer>();
+    kernel.setSerializer(serializer);
+
+    converter.convert.returns({ properties: {} });
+  });
+
+  afterEach(() => {
+    kernel.setSerializer(undefined as any);
+  });
+
+  after(() => {
+    kernel.setConverter(originalConverter);
+  });
+
+  describe('type name', () => {
+    it('returns the default defined type name as class constructor name', () => {
+      @define()
+      class MyClass extends SerializableMixin {}
+
+      expect(new MyClass().getTypeName()).to.be.equal('MyClass');
+      expect(MyClass.getTypeName()).to.be.equal('MyClass');
+    });
+
+    it('returns the defined type name to the type', () => {
+      @define('MyNamedClass')
+      class MyClass extends SerializableMixin {}
+
+      expect(new MyClass().getTypeName()).to.be.equal('MyNamedClass');
+      expect(MyClass.getTypeName()).to.be.equal('MyNamedClass');
+    });
+
+    it('returns the defined type name with namespace', () => {
+      @define('Namespace.MyClass')
+      class MyClass extends SerializableMixin {}
+
+      expect(new MyClass().getTypeName()).to.be.equal('Namespace.MyClass');
+      expect(MyClass.getTypeName()).to.be.equal('Namespace.MyClass');
+    });
+  });
+
+  describe('serialization', () => {
+    it('serializes instance to JSON with serializer', () => {
+      @define('Person', { isRegistrable: false })
+      class Person extends SerializableMixin {}
+
+      const serialized = sinon.stub();
+      const person = new Person();
+      serializer.toJSONValue.returns(serialized);
+      expect(person.toJSONValue()).to.be.eql(serialized);
+      expect(serializer.toJSONValue).to.be.calledOnce;
+      expect(serializer.toJSONValue).to.be.calledWithExactly(person);
+    });
+  });
+});
