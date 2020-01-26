4f646beb8133b784b17d28ae131e672e99d4ab29
Adam Desivi <desivi@desivi.com>
2020-01-26 18:03:45 +0100

    feat(components): add SerializableError

---
+++ src/components/serializable-error.ts
@@ -0,0 +1,94 @@
+import { classes } from 'polytype';
+import merge from 'deepmerge';
+import { pick } from 'lodash';
+import { define } from '../decorators/define';
+import { DefinableMixin } from '../mixins/definable-mixin';
+import { ExtendableError } from './extendable-error';
+import { types } from '../types';
+import { VersionableMixin } from '../mixins/versionable-mixin';
+import { HookableMixin } from '../mixins/hookable-mixin';
+import { isPlainRecord } from '../utils/helpers';
+import { EjsonableMixin } from '../mixins/ejsonable-mixin';
+
+@define('SerializableError')
+export abstract class SerializableError
+  extends classes(
+    ExtendableError,
+    DefinableMixin,
+    HookableMixin,
+    EjsonableMixin,
+    VersionableMixin
+  )
+  implements types.Versionable, types.Ejsonable {
+  name: string;
+
+  message: string;
+
+  stack?: string;
+
+  code?: number;
+
+  schemaVersion: number | undefined;
+
+  /**
+   * Creates an instance of SerializableError.
+   * @param props - Optional properties for error.
+   */
+  constructor(props: types.Props = {}) {
+    const errorProps: types.ErrorProps = pick(props, [
+      'message',
+      'name',
+      'stack',
+      'code',
+    ]);
+    super([errorProps]);
+    Object.assign(this, this.processProps(props));
+  }
+
+  /**
+   * Processes properties for SerializableError.
+   * @param props - Properties of the type required for construction.
+   * @returns Processed properties with any registered `onConstruction` hooks that
+   * valid against prop types.
+   */
+  protected processProps(props: types.Props = {}): types.Props {
+    const processedProps: types.Props = this.onConstruction(props);
+    this.onValidation(processedProps);
+    return processedProps;
+  }
+
+  /**
+   * On construction hook.
+   * @param props - Properties object to be processed.
+   * @returns Processed properties as an object.
+   */
+  protected onConstruction(props: types.Props): types.Props {
+    const propertyInitializers = this.getPropertyInitializers();
+    const processedProps: types.Props = merge(propertyInitializers, props, {
+      isMergeableObject: isPlainRecord,
+    });
+    // Since ExtendableError already assigned error properties during
+    // initializeProperties, we pick that and assign it to processedProps
+    // for validation purposes
+    if (props.name === undefined) processedProps.name = this.name;
+    if (props.message === undefined) processedProps.message = this.message;
+
+    const hooks: types.hooks.Mappings = this.getHooks('onConstruction');
+    for (const hook of Object.values(hooks)) {
+      hook.bind(this)(processedProps);
+    }
+
+    return processedProps;
+  }
+
+  /**
+   * On validation hook.
+   * @param props - Properties object that already has been processed.
+   * @returns Returns `true` on valid properties, else throws.
+   * @throws {ValidationError}
+   * Thrown if the provided properties does not match the prop types.
+   */
+  protected onValidation(props: types.Props): boolean {
+    return this.validateProps(props, this.getPropTypes(), true);
+  }
+}
---
+++ test/unit/components/serializable-error.test.ts
@@ -0,0 +1,172 @@
+import { expect } from 'chai';
+import { instanceOf, PropTypes, ValidationError } from 'typend';
+import { define } from '../../../src/decorators/define';
+import { types } from '../../../src/types';
+import { Serializable } from '../../../src/components/serializable';
+import { SerializableError } from '../../../src/components/serializable-error';
+import { VersionableMixin } from '../../../src/mixins/versionable-mixin';
+import { ExtendableError } from '../../../src/components/extendable-error';
+import { isDefinable } from '../../../src/utils/helpers';
+import { DefinableMixin } from '../../../src/mixins/definable-mixin';
+import { HookableMixin } from '../../../src/mixins/hookable-mixin';
+import { EjsonableMixin } from '../../../src/mixins/ejsonable-mixin';
+import { SerializableMixin } from '../../../src/mixins/serializable-mixin';
+
+describe('SerializableError', function() {
+  @define('MySerializable', { isRegistrable: false })
+  class MySerializable extends Serializable {
+    foo: string;
+  }
+
+  @define('MySerializableError', { isRegistrable: false })
+  class MySerializableError extends SerializableError {}
+
+  @define('MyNestedSerializableError', { isRegistrable: false })
+  class MyNestedSerializableError extends SerializableError {
+    nested: MySerializable;
+  }
+
+  it(`extends ExtendableError`, () => {
+    expect(SerializableError.prototype).to.be.instanceof(ExtendableError);
+  });
+
+  it('implements Definable interface', () => {
+    expect(SerializableError.prototype).to.instanceof(DefinableMixin);
+    expect(instanceOf<types.Definable>(Serializable.prototype)).to.be.true;
+  });
+
+  it('implements Hookable interface', () => {
+    expect(SerializableError.prototype).to.instanceof(HookableMixin);
+    expect(instanceOf<types.Hookable>(Serializable.prototype)).to.be.true;
+  });
+
+  it('implements Serializable interface', () => {
+    expect(SerializableError.prototype).to.instanceof(SerializableMixin);
+    expect(instanceOf<types.Serializable>(Serializable.prototype)).to.be.true;
+  });
+
+  it('implements Ejsonable interface', () => {
+    expect(SerializableError.prototype).to.instanceof(EjsonableMixin);
+    expect(instanceOf<types.Ejsonable>(Serializable.prototype)).to.be.true;
+  });
+
+  it('implements Versionable interface', () => {
+    expect(SerializableError.prototype).to.instanceof(VersionableMixin);
+    expect(instanceOf<types.Versionable>(Serializable.prototype)).to.be.true;
+  });
+
+  it('ensures that type is defined', () => {
+    expect(isDefinable(SerializableError.prototype)).to.be.true;
+  });
+
+  it('defines the type name correctly', () => {
+    expect(SerializableError.getTypeName()).to.equal('SerializableError');
+    expect(SerializableError.prototype.getTypeName()).to.equal(
+      'SerializableError'
+    );
+  });
+
+  describe('property types', () => {
+    it('returns property types as an object', () => {
+      const nested = new MySerializable({
+        foo: 'foo',
+      });
+      const error = new MyNestedSerializableError({
+        message: 'My message',
+        nested,
+      });
+      const propTypes = {
+        name: PropTypes.instanceOf(String),
+        message: PropTypes.instanceOf(String),
+        stack: PropTypes.instanceOf(String).isOptional,
+        code: PropTypes.instanceOf(Number).isOptional,
+        schemaVersion: PropTypes.instanceOf(Number).isOptional,
+        nested: PropTypes.instanceOf(MySerializable),
+      };
+
+      expect(MyNestedSerializableError.getPropTypes()).to.be.eql(propTypes);
+      expect(error.getPropTypes()).to.be.eql(propTypes);
+    });
+  });
+
+  describe('construction', () => {
+    it('throws ValidationError if passed properties on construction does not match prop types', () => {
+      expect(() => {
+        new MyNestedSerializableError({
+          message: 'my-error-message',
+          stack: 'my-stack',
+        });
+      }).to.throw(
+        ValidationError,
+        `MyNestedSerializableError: (Key 'nested': Expected undefined to be a MySerializable in {"message":String("my-error-message"), "stack":String("my-stack"), "name":String("MyNestedSerializableError")})`
+      );
+    });
+
+    it('returns constructed parent class on valid properties', () => {
+      const nested = new MySerializable({
+        foo: 'foo',
+      });
+      const error = new MyNestedSerializableError({
+        message: 'my-error-message',
+        nested,
+      });
+
+      expect(error).to.be.instanceof(MyNestedSerializableError);
+      expect(error).to.be.instanceof(ExtendableError);
+      expect(error.nested).to.be.instanceof(MySerializable);
+      expect(error.nested.foo).to.equal(nested.foo);
+    });
+
+    it(`can be instantiated without passed arguments`, () => {
+      const error = new MySerializableError();
+      expect(error.message).to.be.equal('');
+    });
+
+    it('takes optional message as a string during construction', () => {
+      const message = 'This is a custom message';
+      const error = new MySerializableError({ message });
+      expect(error.message).to.be.equal(message);
+    });
+
+    it(`allows to set message as initalizing class property`, () => {
+      @define('MyDefaultError', { isRegistrable: false })
+      class MyDefaultError extends SerializableError {
+        message = 'The default message for error';
+      }
+
+      expect(new MyDefaultError().message).to.be.equal(
+        'The default message for error'
+      );
+    });
+
+    it('throws the prototype message by default', () => {
+      expect(() => {
+        throw new MySerializableError();
+      }).to.throw(MySerializableError.prototype.message);
+    });
+  });
+
+  describe('hooks', () => {
+    describe('supports onConstruction hook', () => {
+      @define('MyOtherSerializableError', { isRegistrable: false })
+      class MyOtherSerializableError extends SerializableError {}
+
+      const hookFn = (props: types.Props): types.Props => {
+        if (props.message === 'my-error-message') {
+          props.message = 'my-processed-error-message';
+        }
+        return props;
+      };
+      MyOtherSerializableError.prototype.registerHook(
+        'onConstruction',
+        'my-hook',
+        hookFn
+      );
+
+      const instance = new MyOtherSerializableError({
+        message: 'my-error-message',
+      });
+      expect(instance.message).to.be.equal('my-processed-error-message');
+    });
+  });
+});
