4c9939273c394e3ef1b08ba914602efae84db866
Adam Desivi <desivi@desivi.com>
2020-01-26 15:26:24 +0100

    feat: add default constants

---
+++ src/constants/defaults.ts
@@ -0,0 +1,11 @@
+// https://tools.ietf.org/html/rfc5424
+export const LOGGING_LEVELS = {
+  emerg: 0, // Emergency: system is unusable
+  alert: 1, // Alert: action must be taken immediately
+  crit: 2, // Critical: critical conditions
+  error: 3, // Error: error conditions
+  warning: 4, // Warning: warning conditions
+  notice: 5, // Notice: normal but significant condition
+  info: 6, // Informational: informational messages
+  debug: 7, // Debug: debug-level messages
+};
