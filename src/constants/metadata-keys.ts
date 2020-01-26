70f536dc29f1bac7758ac93c6c5791a60d6228b6
Adam Desivi <desivi@desivi.com>
2020-01-26 15:20:23 +0100

    feat: add metadata keys

---
+++ src/constants/metadata-keys.ts
@@ -0,0 +1,27 @@
+export const HOOKABLE_KEY = Symbol('eveble:flags:hookable');
+
+export const HOOKS_CONTAINER_KEY = Symbol('eveble:containers:hooks');
+
+export const DEFAULT_PROPS_KEY = Symbol('eveble:containers:default-props');
+
+export const DELEGATED_KEY = Symbol('eveble:flags:delegated');
+
+export const VERSIONABLE_KEY = Symbol('eveble:versionable');
+
+export const LEGACY_TRANSFORMERS_CONTAINER_KEY = Symbol(
+  'eveble:container:legacy-transformers'
+);
+
+export const HANDLER_KEY = Symbol('eveble:controller:subscriber');
+
+export const HANDLERS_CONTAINER_KEY = Symbol(
+  'eveble:container:subscribing-handlers'
+);
+
+export const SUBSCRIBER_KEY = Symbol('eveble:controller:subscriber');
+
+export const SUBSCRIBING_HANDLERS_CONTAINER_KEY = Symbol(
+  'eveble:container:subscribing-handlers'
+);
+
+export const MESSAGE_METADATA_KEY = Symbol('eveble:container:metadata');
