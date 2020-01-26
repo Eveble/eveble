af8c07d04225c11d5010d15a177fd7fd0c002843
Adam Desivi <desivi@desivi.com>
2020-01-26 17:27:56 +0100

    feat(mixins): add EjsonableMixin

---
+++ src/mixins/ejsonable-mixin.ts
@@ -0,0 +1,22 @@
+import { SerializableMixin } from './serializable-mixin';
+import { types } from '../types';
+
+export class EjsonableMixin extends SerializableMixin {
+  /**
+   * @alias getTypeName
+   * @remarks
+   * Compatibility for EJSON serializer: `@eveble/ejson`
+   */
+  public typeName(): types.TypeName {
+    return this.getTypeName();
+  }
+
+  /**
+   * @alias getTypeName
+   * @remarks
+   * Compatibility for EJSON serializer: `@eveble/ejson`
+   */
+  public static typeName(): types.TypeName {
+    return this.getTypeName();
+  }
+}
---
+++ test/unit/mixins/ejesonable-mixin.test.ts
@@ -0,0 +1,48 @@
+import chai, { expect } from 'chai';
+import { kernel } from '../../../src/core/kernel';
+import { stubInterface } from 'ts-sinon';
+import { types } from '../../../src/types';
+import { define } from '../../../src/decorators/define';
+import sinon from 'sinon';
+import sinonChai from 'sinon-chai';
+import { EjsonableMixin } from '../../../src/mixins/ejsonable-mixin';
+
+chai.use(sinonChai);
+
+describe('EjsonableMixin', () => {
+  let originalConverter: any;
+  let converter: any;
+
+  before(() => {
+    originalConverter = kernel.converter;
+  });
+
+  beforeEach(() => {
+    converter = stubInterface<types.Converter>();
+    kernel.setConverter(converter);
+
+    converter.convert.returns({ properties: {} });
+  });
+
+  after(() => {
+    kernel.setConverter(originalConverter);
+  });
+
+  describe('type name', () => {
+    it('adds typeName static and prototype aliased methods for compatibility @eveble/ejson serializer module', () => {
+      // converter.convert.returns({ properties: {} });
+      @define('MyNamedClass')
+      class MyClass extends EjsonableMixin {}
+      MyClass.getTypeName = sinon.stub();
+
+      const instance = new MyClass();
+      instance.getTypeName = sinon.stub();
+
+      MyClass.typeName();
+      expect(MyClass.getTypeName).to.be.calledOnce;
+
+      instance.typeName();
+      expect(instance.getTypeName).to.be.calledOnce;
+    });
+  });
+});
