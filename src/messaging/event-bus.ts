51b637592e3c9794a575038edaf665d0bd94f46f
Adam Desivi <desivi@desivi.com>
2020-01-26 18:08:51 +0100

    feat(messaging): add EventBus

---
+++ src/messaging/event-bus.ts
@@ -0,0 +1,75 @@
+import { classes } from 'polytype';
+import { types } from '../types';
+import { Event } from '../components/event';
+import { OneToManyHandlingMixin } from '../mixins/one-to-many-handling-mixin';
+import { HookableMixin } from '../mixins/hookable-mixin';
+
+export class EventBus extends classes(HookableMixin, OneToManyHandlingMixin)
+  implements types.EventBus {
+  /**
+   * Creates an instance of CommandBus.
+   */
+  constructor() {
+    super();
+    this.setHandleableTypes([Event]);
+  }
+
+  /**
+   * Registers handler for event.
+   * @param eventType - A subclass of `Event` type.
+   * @param handler - Handler function that will executed upon handling type.
+   * @param shouldOverride - Flag indicating that handler should be overridden if exist.
+   * @throws {UnhandleableTypeError}
+   * Thrown if the type argument is not sublcass of `Event` type.
+   * @throws {InvalidHandlerError}
+   * Thrown if the handler argument is not a function.
+   */
+  registerHandler(
+    eventType: types.MessageableType,
+    handler: types.Handler,
+    shouldOverride = false
+  ): void {
+    super.registerHandler(eventType, handler, shouldOverride);
+  }
+
+  /**
+   * Subscribes to event with handler.
+   * @alias registerHandler
+   */
+  subscribeTo(eventType: types.MessageableType, handler: types.Handler): void {
+    this.registerHandler(eventType, handler);
+  }
+
+  /**
+   * Registers `onPublish` callback hook.
+   * @param id - Identifier under which `onPublish` hook will be defined.
+   * @param hook - Function that will be executed upon publishing `Event`.
+   */
+  onPublish(id: string, hook: types.Hook): void {
+    this.registerHook('onPublish', id, hook);
+  }
+
+  /**
+   * Handles event instance concurrently.
+   * @async
+   * @param eventInstance - An instance of `Event` type.
+   */
+  async handle(eventInstance: Event): Promise<void> {
+    // onPublish hooks always must run even in case when handler is not registered(for BDD testing api on Eveble)
+    const hooks = this.getHooks('onPublish');
+    for (const [, hook] of Object.entries(hooks)) {
+      await hook(eventInstance);
+    }
+
+    return super.handle(eventInstance, 'concurrent');
+  }
+
+  /**
+   * @alias handle
+   * @async
+   */
+  async publish(eventInstance: Event): Promise<void> {
+    const result = await this.handle(eventInstance);
+    return result;
+  }
+}
---
+++ test/unit/messaging/event-bus.test.ts
@@ -0,0 +1,169 @@
+import chai, { expect } from 'chai';
+import { EventBus } from '../../../src/messaging/event-bus';
+import { OneToManyHandlingMixin } from '../../../src/mixins/one-to-many-handling-mixin';
+import { HookableMixin } from '../../../src/mixins/hookable-mixin';
+import sinon from 'sinon';
+import {
+  HandlerExistError,
+  UnhandleableTypeError,
+} from '../../../src/messaging/messaging-errors';
+import { define } from '../../../src/decorators/define';
+import { Command } from '../../../src/components/command';
+import { Event } from '../../../src/components/event';
+import sinonChai from 'sinon-chai';
+import delay from 'delay';
+
+chai.use(sinonChai);
+
+describe('EventBus', function() {
+  @define('MyEvent', { isRegistrable: false })
+  class MyEvent extends Event {
+    key: string;
+  }
+
+  @define('MyCommand', { isRegistrable: false })
+  class MyCommand extends Command {
+    key: string;
+  }
+
+  it(`extends OneToManyHandlingMixin mixin on prototype chain applied`, () => {
+    expect(EventBus.prototype).to.be.instanceof(OneToManyHandlingMixin);
+  });
+
+  it(`extends HookableMixin mixin on prototype chain applied`, () => {
+    expect(EventBus.prototype).to.be.instanceof(HookableMixin);
+  });
+
+  describe('construction', () => {
+    it('sets handleable type upon construction', () => {
+      const eventBus = new EventBus();
+      expect(eventBus.getHandleableTypes()).to.be.eql([Event]);
+    });
+  });
+
+  describe('construction', () => {
+    it('sets handleable type upon construction', () => {
+      const eventBus = new EventBus();
+      expect(eventBus.getHandleableTypes()).to.be.eql([Event]);
+    });
+  });
+
+  describe('handlers registration', () => {
+    it('throws UnhandleabeTypeError when provided type is not handleable', () => {
+      const eventBus = new EventBus();
+      expect(() => eventBus.registerHandler(MyCommand, sinon.stub())).to.throw(
+        UnhandleableTypeError,
+        `EventBus: type must be one of: [Event]; got MyCommand`
+      );
+    });
+
+    it(`allows to register multiple handlers for one event`, () => {
+      const firstHandler = sinon.spy();
+      const secondHandler = sinon.spy();
+
+      const eventBus = new EventBus();
+      eventBus.registerHandler(MyEvent, firstHandler);
+      expect(() => {
+        eventBus.registerHandler(MyEvent, secondHandler);
+      }).to.not.throw(HandlerExistError);
+    });
+
+    it('allows handlers to be overridden', () => {
+      const firstHandler = sinon.spy();
+      const secondHandler = sinon.spy();
+
+      const eventBus = new EventBus();
+      eventBus.registerHandler(MyEvent, firstHandler);
+      eventBus.registerHandler(MyEvent, secondHandler, true);
+
+      expect(eventBus.getHandler(MyEvent)).to.eql([secondHandler]);
+    });
+
+    it('has more expressive api for registering handlers - subscribeTo', () => {
+      const firstHandler = sinon.spy();
+      const eventBus = new EventBus();
+      const registerHandler = sinon.spy(eventBus, 'registerHandler');
+
+      eventBus.subscribeTo(MyEvent, firstHandler);
+
+      expect(registerHandler).to.be.calledOnce;
+      expect(registerHandler).to.be.calledWithExactly(MyEvent, firstHandler);
+    });
+  });
+
+  describe('publishing events', () => {
+    it('handles event', async () => {
+      const handler = sinon.stub();
+      const eventBus = new EventBus();
+      eventBus.registerHandler(MyEvent, handler);
+
+      const event = new MyEvent({
+        sourceId: 'my-target-id',
+        key: 'my-string',
+      });
+      await eventBus.handle(event);
+      expect(handler).to.have.been.calledWithExactly(event);
+    });
+
+    it('ensures that events are handled concurrently', async () => {
+      const firstSpy = sinon.spy();
+      const delayedFirstHandler = async function(eventInstance) {
+        await delay(5);
+        firstSpy(eventInstance);
+      };
+      const secondHandler = sinon.spy();
+
+      const eventBus = new EventBus();
+      eventBus.registerHandler(MyEvent, delayedFirstHandler);
+      eventBus.registerHandler(MyEvent, secondHandler);
+
+      const event = new MyEvent({
+        sourceId: 'my-target-id',
+        key: 'my-string',
+      });
+      await eventBus.handle(event);
+      expect(firstSpy).to.be.calledOnce;
+      expect(firstSpy).to.be.calledWithExactly(event);
+      expect(secondHandler).to.be.calledOnce;
+      expect(secondHandler).to.be.calledWithExactly(event);
+      expect(secondHandler).to.be.calledBefore(firstSpy);
+    });
+
+    it('has more expressive api for handling event - publish', async () => {
+      const handler = sinon.stub();
+      const eventBus = new EventBus();
+      const handle = sinon.spy(eventBus, 'handle');
+
+      eventBus.subscribeTo(MyEvent, handler);
+
+      const event = new MyEvent({
+        sourceId: 'my-target-id',
+        key: 'my-string',
+      });
+      await eventBus.publish(event);
+      expect(handler).to.have.been.calledWithExactly(event);
+      expect(handle).to.be.calledOnce;
+      expect(handle).to.be.calledWithExactly(event);
+    });
+  });
+
+  describe('onPublish hooks', () => {
+    it('calls all hooks when handling an event', async () => {
+      const firstHook = sinon.spy();
+      const secondHook = sinon.spy();
+
+      const eventBus = new EventBus();
+      eventBus.onPublish('first-id', firstHook);
+      eventBus.onPublish('second-id', secondHook);
+      eventBus.subscribeTo(MyEvent, sinon.stub());
+
+      const event = new MyEvent({
+        sourceId: 'my-target-id',
+        key: 'my-string',
+      });
+      await eventBus.publish(event);
+      expect(firstHook).to.have.been.calledWithExactly(event);
+      expect(secondHook).to.have.been.calledWithExactly(event);
+    });
+  });
+});
