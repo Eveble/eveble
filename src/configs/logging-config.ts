3049d6624fa442264a0f63a9b33d76b573920d1f
Adam Desivi <desivi@desivi.com>
2020-01-26 16:58:14 +0100

    feat(configs): add LoggingConfig

---
+++ src/configs/logging-config.ts
@@ -0,0 +1,31 @@
+import { define } from '../decorators/define';
+import { Config } from '../components/config';
+import { LogTransportConfig } from './log-transport-config';
+import { types } from '../types';
+
+@define()
+export class LoggingConfig extends Config {
+  public isEnabled? = true;
+
+  public levels?: types.LogLevels = {
+    emerg: 0,
+    alert: 1,
+    crit: 2,
+    error: 3,
+    warning: 4,
+    notice: 5,
+    info: 6,
+    debug: 7,
+  };
+
+  public transports?: {
+    console?: LogTransportConfig;
+  } = {
+    console: new LogTransportConfig(),
+  };
+
+  constructor(props?: Partial<LoggingConfig>) {
+    super();
+    Object.assign(this, this.processProps(props));
+  }
+}
