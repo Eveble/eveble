66d57560812932fc598b7eda5cf67edbefb0d342
Adam Desivi <desivi@desivi.com>
2020-02-02 15:59:30 +0100

    refactor: implement Publishable, Identifiable interfaces to Event

--- src/components/event.ts
+++ src/components/event.ts
@@ -1,9 +1,11 @@
 import { Message } from './message';
 import { define } from '../decorators/define';
 import { Guid } from '../domain/value-objects/guid';
+import { types } from '../types';
 
 @define('Event')
-export abstract class Event extends Message {
+export abstract class Event extends Message
+  implements types.Publishable, types.Identifiable {
   sourceId: Guid | string;
 
   version?: number;
