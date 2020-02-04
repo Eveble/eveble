a35946edb60091c44bae761988adbdb845f1426e
Adam Desivi <desivi@desivi.com>
2020-02-04 13:27:42 +0100

    test: update DomainError definable unit test

--- test/unit/domain/domain-error.test.ts
+++ test/unit/domain/domain-error.test.ts
@@ -1,10 +1,15 @@
 import { expect } from 'chai';
 import { DomainError } from '../../../src/domain/domain-error';
 import { SerializableError } from '../../../src/components/serializable-error';
+import { isDefinable } from '../../../src/utils/helpers';
 
 describe('DomainError', function() {
   it(`extends SerializableError`, () => {
     expect(DomainError.prototype).to.be.instanceof(SerializableError);
+  });
+
+  it('ensures that type is defined', () => {
+    expect(isDefinable(DomainError.prototype)).to.be.true;
   });
 
   it('defines its serializable type name correctly', () => {
