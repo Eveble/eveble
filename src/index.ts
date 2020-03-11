af4ff0cbe948f5f7370ffe1a29033a44a5eae60f
Adam Desivi <desivi@desivi.com>
2020-03-11 20:00:14 +0100

    build: add exports

---
+++ src/index.ts
@@ -0,0 +1,310 @@
+/*
+EVEBLE
+*/
+// Annotations
+export { delegate } from './annotations/delegate';
+export { handle } from './annotations/handle';
+export { initial } from './annotations/initial';
+export { route } from './annotations/route';
+export { subscribe } from './annotations/subscribe';
+export { version } from './annotations/version';
+// App
+export { App } from './app/app';
+export { Eveble } from './app/eveble';
+export { AgendaCommandSchedulerModule } from './app/modules/agenda-command-scheduler-module';
+export { MongoDBCommitStorageModule } from './app/modules/mongodb-commit-storage-module';
+export { MongoDBSnapshotStorageModule } from './app/modules/mongodb-snapshot-storage-module';
+// Components
+export { Command, Assignment } from './components/command';
+export { Config } from './components/config';
+export { Event } from './components/event';
+export { ExtendableError } from './components/extendable-error';
+export { Log, LogMetadata } from './components/log-entry';
+export { Message } from './components/message';
+export { PickableProperties } from './components/pickable-properties';
+export { SerializableError } from './components/serializable-error';
+export { Serializable } from './components/serializable';
+export { Struct } from './components/struct';
+// Config
+export { AppConfig } from './configs/app-config';
+export { EvebleConfig } from './configs/eveble-config';
+export { LoggingConfig } from './configs/logging-config';
+export { LogTransportConfig } from './configs/log-transport-config';
+// Constants
+export { BINDINGS } from './constants/bindings';
+// Prettier does not like export * as SOMETHING from '..';
+export { LITERAL_KEYS } from './constants/literal-keys';
+export { DEFAULTS } from './constants/defaults';
+export { METADATA_KEYS } from './constants/metadata-keys';
+export { SPECIFICATIONS } from './constants/specifications';
+// Core
+export { StringifingConverter } from './core/logging-transports/formatters/converters/stringifing-converter';
+export { DetailedLogFormatter } from './core/logging-transports/formatters/detailed-log-entry-formatter';
+export { SimpleLogFormatter } from './core/logging-transports/formatters/simple-log-entry-formatter';
+export { ConsoleTransport } from './core/logging-transports/console-transport';
+export { BaseApp } from './core/base-app';
+export {
+  InjectorError,
+  InvalidEventSourceableError,
+  KernelError,
+  UnavailableSerializerError,
+  UnavailableAsserterError,
+  TypeError,
+  TypeExistsError,
+  TypeNotFoundError,
+  UnregistrableTypeError,
+  ModuleError,
+  AppMissingError,
+  InjectorMissingError,
+  InvalidModuleError,
+  InvalidConfigError,
+  InvalidEnvironmentError,
+  AppError,
+  InvalidAppConfigError,
+  LoggingError,
+  InvalidTransportIdError,
+  TransportExistsError,
+} from './core/core-errors';
+export { Container } from './core/injector';
+export { Kernel, kernel } from './core/kernel';
+export { Library } from './core/library';
+export { LogTransport } from './core/log-transport';
+export { Logger } from './core/logger';
+export { Module } from './core/module';
+// Decorators
+export { define } from './decorators/define';
+// Domain
+export { AbilityAssertion } from './domain/assertions/ability-assertion';
+export {
+  StatefulAssertion,
+  InvalidStateTransitionError,
+} from './domain/assertions/stateful-assertion';
+export {
+  StatusfulAssertion,
+  InvalidStatusTransitionError,
+} from './domain/assertions/statusful-assertion';
+export { Guid } from './domain/value-objects/guid';
+export { Aggregate } from './domain/aggregate';
+export { Asserter, AssertionApiAlreadyExistsError } from './domain/asserter';
+export { Assertion } from './domain/assertion';
+export { BoundedContext } from './domain/bounded-context';
+export { DomainError } from './domain/domain-error';
+export {
+  ListError,
+  IdentifiableAlreadyExistsError,
+  ElementAlreadyExistsError,
+  ElementNotFoundError,
+  InvalidListError,
+  ValueObjectError,
+  EntityError,
+  SavedStateNotFoundError,
+  EventSourceableError,
+  InvalidEventError,
+  EventIdMismatchError,
+  InvalidInitializingMessageError,
+} from './domain/domain-errors';
+export { DomainException } from './domain/domain-exception';
+export { Entity } from './domain/entity';
+export { EventSourceable } from './domain/event-sourceable';
+export { History } from './domain/history';
+export { List } from './domain/list';
+export { ScheduleCommand } from './domain/schedule-command';
+export { UnscheduleCommand } from './domain/unschedule-command';
+export { Process } from './domain/process';
+export { ValueObject } from './domain/value-object';
+// Infrastructure
+export { AgendaClient } from './infrastructure/clients/agenda-client';
+export { MongoDBClient } from './infrastructure/clients/mongodb-client';
+export { AgendaCommandScheduler } from './infrastructure/schedulers/agenda-command-scheduler';
+export { CommitSerializer } from './infrastructure/serializers/commit-serializer';
+export { SnapshotSerializer } from './infrastructure/serializers/snapshot-serializer';
+export { CommitMongoDBObserver } from './infrastructure/storages/commit-mongodb-observer';
+export { CommitMongoDBStorage } from './infrastructure/storages/commit-mongodb-storage';
+export { SnapshotMongoDBStorage } from './infrastructure/storages/snapshot-mongodb-storage';
+export { Commit, CommitReceiver } from './infrastructure/structs/commit';
+export { ScheduledJob } from './infrastructure/structs/scheduled-job';
+export { AgendaScheduledJobTransformer } from './infrastructure/transformers/agenda-scheduled-job-transformer';
+export { Client } from './infrastructure/client';
+export { CommandSchedulingService } from './infrastructure/command-scheduling-service';
+export { CommitPublisher } from './infrastructure/commit-publisher';
+export { CommitStore } from './infrastructure/commit-store';
+export { EventSourceableRepository } from './infrastructure/event-sourceable-repository';
+export {
+  InfrastructureError,
+  CommitConcurrencyError,
+  EventsNotFoundError,
+  AddingCommitFailedError,
+  UpdatingCommitError,
+  AddingSnapshotError,
+  UpdatingSnapshotError,
+  StorageNotFoundError,
+  RouterError,
+  MissingEventSourceableError,
+  MissingInitializingMessageError,
+  CannotRouteMessageError,
+  UnresolvableIdentifierFromMessageError,
+  UndefinedSnapshotterFrequencyError,
+  UndefinedSnapshotterError,
+  ProjectionRebuildingError,
+  ProjectionAlreadyRebuildingError,
+  ProjectionNotRebuildingError,
+  ClientError,
+  InactiveClientError,
+  SchedulerError,
+  CommandSchedulingError,
+  CommandUnschedulingError,
+} from './infrastructure/infrastructure-errors';
+export { ProjectionRebuilder } from './infrastructure/projection-rebuilder';
+export { Projection } from './infrastructure/projection';
+export { Router } from './infrastructure/router';
+export { Service } from './infrastructure/service';
+export { Snapshotter } from './infrastructure/snapshotter';
+// Messaging
+export { EJSONSerializerAdapter } from './messaging/serializers/ejson-serializer-adapter';
+export { CommandBus } from './messaging/command-bus';
+export { EventBus } from './messaging/event-bus';
+export {
+  HandlingError,
+  UnhandleableTypeError,
+  InvalidControllerError,
+  InvalidHandlerError,
+  HandlerExistError,
+  HandlerNotFoundError,
+  UnsupportedExecutionTypeError,
+  InvalidMessageableType,
+  InitializingMessageAlreadyExistsError,
+  SerializationError,
+  UnparsableValueError,
+} from './messaging/messaging-errors';
+// Mixins
+export { CommandHandlingMixin } from './mixins/command-handling-mixin';
+export { DefinableMixin } from './mixins/definable-mixin';
+export { EjsonableMixin } from './mixins/ejsonable-mixin';
+export { EventHandlingMixin } from './mixins/event-handling-mixin';
+export {
+  HookableMixin,
+  HookError,
+  InvalidHookActionError,
+  InvalidHookIdError,
+  HookAlreadyExistsError,
+  HookNotFoundError,
+} from './mixins/hookable-mixin';
+export { OneToManyHandlingMixin } from './mixins/one-to-many-handling-mixin';
+export { OneToOneHandlingMixin } from './mixins/one-to-one-handling-mixin';
+export { RFC5424LoggingMixin } from './mixins/rfc-5424-logging-mixin';
+export { SerializableMixin } from './mixins/serializable-mixin';
+export {
+  StatefulMixin,
+  StateError,
+  UndefinedStatesError,
+  InvalidStateError,
+} from './mixins/stateful-mixin';
+export {
+  StatusfulMixin,
+  StatusError,
+  UndefinedStatusesError,
+  InvalidStatusError,
+} from './mixins/statusful-mixin';
+export {
+  VersionableMixin,
+  VersionableError,
+  InvalidSchemaVersionError,
+  LegacyTransformerAlreadyExistsError,
+  LegacyTransformerNotFoundError,
+  InvalidLegacyTransformerError,
+  NotVersionableError,
+} from './mixins/versionable-mixin';
+// Helpers
+export {
+  isDefinable,
+  isSerializable,
+  isRecord,
+  isPlainRecord,
+  hasPostConstruct,
+  toPlainObject,
+  convertObjectToCollection,
+  resolveSerializableFromPropType,
+  createEJSON,
+  isEventSourceableType,
+} from './utils/helpers';
+// Types
+export { types } from './types';
+export { types as EvebleTypes } from './types';
+export {
+  postConstruct,
+  injectable,
+  inject,
+} from '@parisholley/inversify-async';
+export { Container as Injector } from './core/injector';
+export { bootstrapLogger } from './utils/logger-bootstrapper';
+/*
+TYPEND
+*/
+export {
+  // Utility types,
+  $PropsOf,
+  $TypeOf,
+  // Errors
+  ValidationError,
+  InvalidDefinitionError,
+  InvalidTypeError,
+  InvalidValueError,
+  UnequalValueError,
+  UnmatchedTypeError,
+  NotAMemberError,
+  UnexpectedKeyError,
+  UnknownError,
+  TypeDescriberExistsError,
+  TypeDescriberNotFoundError,
+  PatternValidatorExistError,
+  PatternValidatorNotFoundError,
+  UndefinableClassError,
+  TypeConverterExists,
+  UndefinedConverterError,
+  // Utilities
+  PropsOf,
+  TypeOf,
+  // Typend end-api
+  typend,
+  validator,
+  describer,
+  converter,
+  validate,
+  isValid,
+  isInstanceOf,
+  check,
+  is,
+  instanceOf,
+  convert,
+  reflect,
+  // Annotations
+  internal,
+  validable,
+  // Patterns
+  any,
+  iof,
+  collection,
+  collectionIncluding,
+  collectionWithin,
+  eq,
+  integer,
+  list,
+  maybe,
+  never,
+  oneOf,
+  optional,
+  tuple,
+  unknown,
+  unrecognized,
+  voided,
+  where,
+  PropTypes,
+  // Utilities
+  propsOf,
+  typeOf,
+  // Primitive types
+  string,
+  number,
+  boolean,
+  symbol,
+} from 'typend';
