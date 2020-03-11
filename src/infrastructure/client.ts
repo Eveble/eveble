1d52de1ebedbd35769c5ba6329edd5e2601bc4f8
Adam Desivi <desivi@desivi.com>
2020-03-11 17:20:10 +0100

    feat(infrastructure): add Client

---
+++ src/infrastructure/client.ts
@@ -0,0 +1,45 @@
+import { classes } from 'polytype';
+import { Guid } from '../domain/value-objects/guid';
+import { types } from '../types';
+import { Struct } from '../components/struct';
+import { StatefulMixin } from '../mixins/stateful-mixin';
+import { define } from '../decorators/define';
+
+@define()
+export class Client extends classes(Struct, StatefulMixin) {
+  static STATES = {
+    constructed: 'constructed',
+    initialized: 'initialized',
+    connected: 'connected',
+    paused: 'paused',
+    stopped: 'stopped',
+    disconnected: 'disconnected',
+    failed: 'failed',
+  };
+
+  public id: string | Guid;
+
+  public state: types.State;
+
+  /**
+   * Creates an instance of Client.
+   * @param props - Properties of the type required for construction.
+   * @remarks
+   * Since were dealing with special cases, mixins and limits of TypeScript, we
+   * use of "invoking multiple base constructors" from polytype to pass props to Struct's
+   * constructor:
+   * https://www.npmjs.com/package/polytype#invoking-multiple-base-constructors
+   */
+  constructor(props: types.Props = {}) {
+    super([props]);
+    this.setState(Client.STATES.constructed);
+  }
+
+  /**
+   * Returns client identifier.
+   * @return Client's identifier.
+   */
+  public getId(): string | Guid {
+    return this.id;
+  }
+}
