5e8d62288c6cd8efd1b854cbc4e8ed8bee4cd64d
Adam Desivi <desivi@desivi.com>
2020-03-11 19:40:50 +0100

    refactor(configs): AppConfig

--- test/unit/configs/app-config.test.ts
+++ test/unit/configs/app-config.test.ts
@@ -4,11 +4,15 @@ import sinon from 'sinon';
 import { AppConfig } from '../../../src/configs/app-config';
 import { Config } from '../../../src/components/config';
 import { LoggingConfig } from '../../../src/configs/logging-config';
+import { EvebleConfig } from '../../../src/configs/eveble-config';
 
 describe('AppConfig', function() {
   let generateId: any;
+  const id = 'my-id';
+
   beforeEach(() => {
     generateId = sinon.stub(AppConfig, 'generateId');
+    generateId.returns(id);
   });
 
   afterEach(() => {
@@ -22,6 +26,12 @@ describe('AppConfig', function() {
   describe('prop types', () => {
     it('takes optional appId property as a string', () => {
       expect(AppConfig.getPropTypes().appId).to.be.eql(
+        PropTypes.instanceOf(String).isOptional
+      );
+    });
+
+    it('takes optional workerId property as a string', () => {
+      expect(AppConfig.getPropTypes().workerId).to.be.eql(
         PropTypes.instanceOf(String).isOptional
       );
     });
@@ -77,18 +87,94 @@ describe('AppConfig', function() {
         }).isOptional
       );
     });
+
+    it(`takes optional clients property as an object with options properties for clients`, () => {
+      expect(AppConfig.getPropTypes().clients).to.be.eql(
+        PropTypes.shape({
+          MongoDB: PropTypes.shape({
+            CommitStore: PropTypes.object.isOptional,
+            Snapshotter: PropTypes.object.isOptional,
+            CommandScheduler: PropTypes.object.isOptional,
+          }).isOptional,
+          Agenda: PropTypes.shape({
+            CommandScheduler: PropTypes.object.isOptional,
+          }).isOptional,
+        }).isOptional
+      );
+    });
+
+    it('takes optional eveble property as an instance of EvebleConfig', () => {
+      expect(AppConfig.getPropTypes().eveble).to.be.eql(
+        PropTypes.instanceOf(EvebleConfig).isOptional
+      );
+    });
   });
 
-  describe('defaults', () => {
-    it('has default values assigned via property initializers', () => {
-      const id = 'my-id';
-      generateId.returns(id);
-      expect(new AppConfig()).to.be.eql({
-        logging: new LoggingConfig(),
-        appId: id,
-        conversion: { type: 'runtime' },
-        validation: { type: 'runtime' },
-        description: { formatting: 'default' },
+  describe('construction', () => {
+    describe('defaults via property initializers', () => {
+      it('has default values for logging property', () => {
+        expect(new AppConfig().logging).to.be.instanceof(LoggingConfig);
+        expect(new AppConfig().logging).to.be.eql(new LoggingConfig());
+      });
+
+      it('has default values for appId property as generated uuid', () => {
+        expect(new AppConfig().appId).to.be.equal(id);
+      });
+
+      it('has default values for workerId property as generated uuid', () => {
+        expect(new AppConfig().workerId).to.be.equal(id);
+      });
+
+      it('has default values for conversion property', () => {
+        expect(new AppConfig().conversion).to.be.eql({ type: 'runtime' });
+      });
+
+      it('has default values for validation property', () => {
+        expect(new AppConfig().validation).to.be.eql({ type: 'runtime' });
+      });
+
+      it('has default values for description property', () => {
+        expect(new AppConfig().description).to.be.eql({
+          formatting: 'default',
+        });
+      });
+
+      it('has default values for clients property', () => {
+        const defaultMongoDBOptions = {
+          useNewUrlParser: true,
+          useUnifiedTopology: true,
+        };
+        expect(new AppConfig().clients).to.be.eql({
+          MongoDB: {
+            CommitStore: defaultMongoDBOptions,
+            Snapshotter: defaultMongoDBOptions,
+            CommandScheduler: defaultMongoDBOptions,
+          },
+          Agenda: {
+            CommandScheduler: {
+              processEvery: 180000,
+            },
+          },
+        });
+      });
+
+      it('has default values for eveble property', () => {
+        expect(new AppConfig().eveble).to.be.instanceof(EvebleConfig);
+        expect(new AppConfig().eveble).to.be.eql(new EvebleConfig());
+      });
+    });
+
+    describe('default values via ENV property initializers', () => {
+      it('uses env APP_ID value for appId property', () => {
+        const envId = 'app-env-id';
+        process.env.APP_ID = envId;
+        expect(new AppConfig().appId).to.be.equal(envId);
+      });
+
+      it('uses env WORKER_ID value for workerId property', () => {
+        const envId = 'worker-env-id';
+        process.env.WORKER_ID = envId;
+        expect(new AppConfig().workerId).to.be.equal(envId);
       });
     });
   });
