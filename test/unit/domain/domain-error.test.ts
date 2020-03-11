9bb402502c435e9ca8ea802f7b427ca68811e2e5
Adam Desivi <desivi@desivi.com>
2020-03-11 15:10:40 +0100

    refactor: ensure that DomainError is an abstract class

--- test/unit/domain/domain-error.test.ts
+++ test/unit/domain/domain-error.test.ts
@@ -2,8 +2,12 @@ import { expect } from 'chai';
 import { DomainError } from '../../../src/domain/domain-error';
 import { SerializableError } from '../../../src/components/serializable-error';
 import { isDefinable } from '../../../src/utils/helpers';
+import { define } from '../../../src/decorators/define';
 
 describe('DomainError', function() {
+  @define()
+  class MyDomainError extends DomainError {}
+
   it(`extends SerializableError`, () => {
     expect(DomainError.prototype).to.be.instanceof(SerializableError);
   });
@@ -14,6 +18,8 @@ describe('DomainError', function() {
 
   it('defines its serializable type name correctly', () => {
     expect(DomainError.getTypeName()).to.equal('DomainError');
-    expect(new DomainError('my-error').getTypeName()).to.equal('DomainError');
+    expect(new MyDomainError('my-error').getTypeName()).to.equal(
+      'MyDomainError'
+    );
   });
 });
