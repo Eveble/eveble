e109d0ce3738b5968564995b892b7c161b2f6e62
Adam Desivi <desivi@desivi.com>
2020-03-11 14:18:59 +0100

    refactor: ensure metadata keys symbol uniqueness

--- src/constants/metadata-keys.ts
+++ src/constants/metadata-keys.ts
@@ -1,38 +1,44 @@
 /*
 CORE
 */
-export const HOOKABLE_KEY = Symbol('eveble:flags:hookable');
+export const HOOKABLE_KEY: unique symbol = Symbol('eveble:flags:hookable');
 
-export const HOOKS_CONTAINER_KEY = Symbol('eveble:containers:hooks');
+export const HOOKS_CONTAINER_KEY: unique symbol = Symbol(
+  'eveble:containers:hooks'
+);
 
-export const DEFAULT_PROPS_KEY = Symbol('eveble:containers:default-props');
+export const DEFAULT_PROPS_KEY: unique symbol = Symbol(
+  'eveble:containers:default-props'
+);
 
-export const DELEGATED_KEY = Symbol('eveble:flags:delegated');
+export const DELEGATED_KEY: unique symbol = Symbol('eveble:flags:delegated');
 
-export const VERSIONABLE_KEY = Symbol('eveble:versionable');
+export const VERSIONABLE_KEY: unique symbol = Symbol('eveble:versionable');
 
-export const LEGACY_TRANSFORMERS_CONTAINER_KEY = Symbol(
+export const LEGACY_TRANSFORMERS_CONTAINER_KEY: unique symbol = Symbol(
   'eveble:container:legacy-transformers'
 );
 
 /*
 HANDLING
 */
-export const HANDLER_KEY = Symbol('eveble:controller:handler');
+export const HANDLER_KEY: unique symbol = Symbol('eveble:controller:handler');
 
-export const COMMAND_HANDLERS_CONTAINER_KEY = Symbol(
+export const COMMAND_HANDLERS_CONTAINER_KEY: unique symbol = Symbol(
   'eveble:container:command-handlers'
 );
 
-export const SUBSCRIBER_KEY = Symbol('eveble:controller:subscriber');
+export const SUBSCRIBER_KEY: unique symbol = Symbol(
+  'eveble:controller:subscriber'
+);
 
-export const EVENT_HANDLERS_CONTAINER_KEY = Symbol(
+export const EVENT_HANDLERS_CONTAINER_KEY: unique symbol = Symbol(
   'eveble:container:event-handlers'
 );
 
 /*
 SERIALIZABLE
 */
-export const SERIALIZABLE_LIST_PROPS_KEY = Symbol(
+export const SERIALIZABLE_LIST_PROPS_KEY: unique symbol = Symbol(
   'eveble:container:serializable-list-props'
 );
