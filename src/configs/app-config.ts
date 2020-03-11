dd4056ed0eda5f480b16fb45a5b6969fd4daf80e
Adam Desivi <desivi@desivi.com>
2020-03-11 19:40:50 +0100

    refactor(configs): AppConfig

--- src/configs/app-config.ts
+++ src/configs/app-config.ts
@@ -1,11 +1,27 @@
-import uuid from 'uuid';
+import { v4 as uuidv4 } from 'uuid';
+import getenv from 'getenv';
 import { define } from '../decorators/define';
 import { Config } from '../components/config';
 import { LoggingConfig } from './logging-config';
+import { EvebleConfig } from './eveble-config';
+import { types } from '../types';
 
 @define()
 export class AppConfig extends Config {
-  public appId?: string = AppConfig.generateId();
+  static defaultMongoDBOptions = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  };
+
+  public appId?: string = getenv.string(
+    'APP_ID',
+    AppConfig.generateId() as string
+  );
+
+  public workerId?: string = getenv.string(
+    'WORKER_ID',
+    AppConfig.generateId() as string
+  );
 
   public logging?: LoggingConfig = new LoggingConfig();
 
@@ -21,9 +37,50 @@ export class AppConfig extends Config {
     formatting: 'default' | 'compact' | 'debug';
   } = { formatting: 'default' };
 
+  public eveble?: EvebleConfig = new EvebleConfig();
+
+  public clients?: {
+    MongoDB?: {
+      CommitStore?: Record<string, any>; // MongoClientOptions, tsruntime fails to identify kind
+      Snapshotter?: Record<string, any>; // MongoClientOptions, tsruntime fails to identify kind
+      CommandScheduler?: Record<string, any>; // MongoClientOptions, tsruntime fails to identify kind
+    };
+    Agenda?: {
+      CommandScheduler?: Record<string, any>; // AgendaConfiguration, tsruntime fails to identify kind
+    };
+  } = {
+    MongoDB: {
+      CommitStore: AppConfig.defaultMongoDBOptions,
+      Snapshotter: AppConfig.defaultMongoDBOptions,
+      CommandScheduler: AppConfig.defaultMongoDBOptions,
+    },
+    Agenda: {
+      CommandScheduler: {
+        processEvery: 180000,
+      },
+    },
+  };
+
+  /**
+   * Creates an instance of AppConfig.
+   * @param props - Properties of the type required for construction.
+   */
   constructor(props?: Partial<AppConfig>) {
     super();
     if (props) Object.assign(this, this.processProps(props));
+  }
+
+  /**
+   * Processes properties for AppConfig.
+   * @param props - Properties of the type required for construction.
+   * @returns Processed properties with any registered `onConstruction` hooks and
+   * validates them against prop types.
+   */
+  protected processProps(props: types.Props = {}): types.Props {
+    if (props.eveble !== undefined && !(props.eveble instanceof EvebleConfig)) {
+      props.eveble = new EvebleConfig(props.eveble);
+    }
+    return super.processProps(props);
   }
 
   /**
@@ -32,6 +89,6 @@ export class AppConfig extends Config {
    * @returns String identifier as uuid.
    */
   public static generateId(): string {
-    return uuid.v4().toString();
+    return uuidv4().toString();
   }
 }
