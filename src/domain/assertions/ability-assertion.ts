479295d5829856fe7ce029611bd11665d60d2dcd
Adam Desivi <desivi@desivi.com>
2020-04-06 23:10:31 +0200

    refactor(domain): AbilityAssertion to more expressive api

--- src/domain/assertions/ability-assertion.ts
+++ src/domain/assertions/ability-assertion.ts
@@ -1,47 +1,62 @@
 import { Assertion } from '../assertion';
-import { types } from '../../types';
 import {
   SAVE_STATE_METHOD_KEY,
   ROLLBACK_STATE_METHOD_KEY,
 } from '../../constants/literal-keys';
 
 export class AbilityAssertion extends Assertion {
-  public api: Map<string, Function> = new Map([
-    ['ensure.is.ableTo', this.ensureIsAbleTo as Function],
-    ['is.ableTo', this.isAbleTo as Function],
+  /**
+   * Since our goal is to enable expressive API in form of:
+   * @example
+   *```ts
+   * entity.ensure.is.ableTo.doAction(...)
+   *```
+   * We need to use Proxy to pass-through all calls to the entity itself
+   * (entity instance will be not set upon assertion registration)
+   */
+  public api: Map<string, any> = new Map([
+    [
+      'ensure.is.ableTo',
+      new Proxy(this, {
+        get(target: any, propKey: string): any {
+          const entity = target.asserter.getEntity();
+          if (typeof entity[propKey] === 'function') {
+            const proxifiedMethod = new Proxy(entity[propKey], {
+              apply(_targetMethod, _thisArg, args): any {
+                entity[SAVE_STATE_METHOD_KEY]();
+                const result = entity[propKey](...args);
+                entity[ROLLBACK_STATE_METHOD_KEY]();
+                return result;
+              },
+            });
+            return proxifiedMethod;
+          }
+        },
+      }),
+    ],
+    [
+      'is.ableTo',
+      new Proxy(this, {
+        get(target: any, propKey: string): any {
+          const entity = target.asserter.getEntity();
+          if (typeof entity[propKey] === 'function') {
+            const proxifiedMethod = new Proxy(entity[propKey], {
+              apply(_targetMethod, _thisArg, args): any {
+                entity[SAVE_STATE_METHOD_KEY]();
+                let isAble = true;
+                try {
+                  entity[propKey](...args);
+                } catch (e) {
+                  isAble = false;
+                }
+                entity[ROLLBACK_STATE_METHOD_KEY]();
+                return isAble;
+              },
+            });
+            return proxifiedMethod;
+          }
+        },
+      }),
+    ],
   ]);
-
-  /**
-   * Ensures that `Entity` is able to handle an action.
-   * @param args - Arguments that should be passed to action on `Entity`.
-   * @returns `Asserter` implementation instance.
-   */
-  ensureIsAbleTo(...args: any[]): types.Asserter {
-    const action = this.asserter.getAction();
-    const entity = this.asserter.getEntity();
-
-    entity[SAVE_STATE_METHOD_KEY]();
-    entity[action.toString()](...args);
-    entity[ROLLBACK_STATE_METHOD_KEY]();
-    return this.asserter;
-  }
-
-  /**
-   * Ensures that `Entity` is able to handle an action.
-   * @param args - Arguments that should be passed to action on `Entity`.
-   * @returns `Asserter` implementation instance.
-   */
-  isAbleTo(...args: any[]): boolean {
-    const action = this.asserter.getAction();
-    const entity = this.asserter.getEntity();
-    entity[SAVE_STATE_METHOD_KEY]();
-    let isAble = true;
-    try {
-      entity[action.toString()](...args);
-    } catch (e) {
-      isAble = false;
-    }
-    entity[ROLLBACK_STATE_METHOD_KEY]();
-    return isAble;
-  }
 }
--- test/unit/domain/assertions/ability-assertion.test.ts
+++ test/unit/domain/assertions/ability-assertion.test.ts
@@ -4,6 +4,7 @@ import sinonChai from 'sinon-chai';
 import sinon from 'sinon';
 import { types } from '../../../../src/types';
 import { AbilityAssertion } from '../../../../src/domain/assertions/ability-assertion';
+import { Asserter } from '../../../../src/domain/asserter';
 import {
   SAVE_STATE_METHOD_KEY,
   ROLLBACK_STATE_METHOD_KEY,
@@ -17,23 +18,22 @@ describe(`AbilityAssertion`, () => {
 
   beforeEach(() => {
     entity = stubInterface<types.Entity>();
-    asserter = stubInterface<types.Asserter>();
-
-    asserter.getEntity.returns(entity);
+    asserter = new Asserter();
+    asserter.setEntity(entity);
   });
 
   describe('extends asserter API', () => {
     it('ensure.is.ableTo', () => {
       const assertion = new AbilityAssertion(asserter);
-      expect(assertion.getApi().get('ensure.is.ableTo')).to.be.equal(
-        assertion.ensureIsAbleTo
+      expect(assertion.getApi().get('ensure.is.ableTo')).to.be.instanceof(
+        AbilityAssertion
       );
     });
 
     it('is.ableTo', () => {
       const assertion = new AbilityAssertion(asserter);
-      expect(assertion.getApi().get('is.ableTo')).to.be.equal(
-        assertion.isAbleTo
+      expect(assertion.getApi().get('is.ableTo')).to.be.instanceof(
+        AbilityAssertion
       );
     });
   });
@@ -41,13 +41,14 @@ describe(`AbilityAssertion`, () => {
   describe('ensures ability to change state', () => {
     it('ensures that state of entity is being snapshotted before validation', () => {
       const assertion = new AbilityAssertion(asserter);
+      asserter.registerAssertion(assertion);
       const args = [1, 2, 3, 4];
 
       entity.myAction = sinon.stub();
-      asserter.getAction.returns('myAction');
-      entity[SAVE_STATE_METHOD_KEY] = sinon.stub();
+      entity.myAction.returns('result');
 
-      expect(assertion.ensureIsAbleTo(...args)).to.be.equal(asserter);
+      entity[SAVE_STATE_METHOD_KEY] = sinon.stub();
+      expect(asserter.ensure.is.ableTo.myAction(...args)).to.be.equal('result');
       expect(entity.myAction).to.be.calledOnce;
       expect(entity.myAction).to.be.calledWithExactly(...args);
       expect(entity[SAVE_STATE_METHOD_KEY]).to.be.calledBefore(entity.myAction);
@@ -55,12 +56,13 @@ describe(`AbilityAssertion`, () => {
 
     it('ensures that state of entity is being rollbacked after validation', () => {
       const assertion = new AbilityAssertion(asserter);
+      asserter.registerAssertion(assertion);
       const args = [1, 2, 3, 4];
 
       entity.myAction = sinon.stub();
-      asserter.getAction.returns('myAction');
+      entity.myAction.returns('result');
 
-      expect(assertion.ensureIsAbleTo(...args)).to.be.equal(asserter);
+      expect(asserter.ensure.is.ableTo.myAction(...args)).to.be.equal('result');
       expect(entity.myAction).to.be.calledOnce;
       expect(entity.myAction).to.be.calledWithExactly(...args);
       expect(entity[ROLLBACK_STATE_METHOD_KEY]).to.be.calledAfter(
@@ -72,39 +74,39 @@ describe(`AbilityAssertion`, () => {
   describe('evaluation of ability to change state', () => {
     it('returns true for successful evaluation', () => {
       const assertion = new AbilityAssertion(asserter);
+      asserter.registerAssertion(assertion);
       const args = [1, 2, 3, 4];
 
       entity.myAction = sinon.stub();
       entity.myAction.returns(true);
-      asserter.getAction.returns('myAction');
 
-      expect(assertion.isAbleTo(...args)).to.be.true;
+      expect(asserter.is.ableTo.myAction(...args)).to.be.true;
       expect(entity.myAction).to.be.calledOnce;
       expect(entity.myAction).to.be.calledWithExactly(...args);
     });
 
     it('ensures that state of entity is being snapshotted before evaluation', () => {
       const assertion = new AbilityAssertion(asserter);
+      asserter.registerAssertion(assertion);
       const args = [1, 2, 3, 4];
 
       entity.myAction = sinon.stub();
       entity.myAction.returns(true);
-      asserter.getAction.returns('myAction');
       entity[SAVE_STATE_METHOD_KEY] = sinon.stub();
 
-      expect(assertion.isAbleTo(...args)).to.be.true;
+      expect(asserter.is.ableTo.myAction(...args)).to.be.true;
       expect(entity[SAVE_STATE_METHOD_KEY]).to.be.calledBefore(entity.myAction);
     });
 
     it('ensures that state of entity is being rollbacked after validation', () => {
       const assertion = new AbilityAssertion(asserter);
+      asserter.registerAssertion(assertion);
       const args = [1, 2, 3, 4];
 
       entity.myAction = sinon.stub();
       entity.myAction.returns(true);
-      asserter.getAction.returns('myAction');
 
-      expect(assertion.isAbleTo(...args)).to.be.true;
+      expect(asserter.is.ableTo.myAction(...args)).to.be.true;
       expect(entity[ROLLBACK_STATE_METHOD_KEY]).to.be.calledAfter(
         entity.myAction
       );
@@ -112,26 +114,26 @@ describe(`AbilityAssertion`, () => {
 
     it('returns false for failed evaluation', () => {
       const assertion = new AbilityAssertion(asserter);
+      asserter.registerAssertion(assertion);
       const args = [1, 2, 3, 4];
 
       entity.myAction = sinon.stub();
       entity.myAction.throws(new Error());
-      asserter.getAction.returns('myAction');
 
-      expect(assertion.isAbleTo(...args)).to.be.false;
+      expect(asserter.is.ableTo.myAction(...args)).to.be.false;
       expect(entity.myAction).to.be.calledOnce;
       expect(entity.myAction).to.be.calledWithExactly(...args);
     });
 
     it('ensures that state of entity is being rollbacked if action throws error', () => {
       const assertion = new AbilityAssertion(asserter);
+      asserter.registerAssertion(assertion);
       const args = [1, 2, 3, 4];
 
       entity.myAction = sinon.stub();
       entity.myAction.throws(new Error());
-      asserter.getAction.returns('myAction');
 
-      expect(assertion.isAbleTo(...args)).to.be.false;
+      expect(asserter.is.ableTo.myAction(...args)).to.be.false;
       expect(entity[ROLLBACK_STATE_METHOD_KEY]).to.be.calledAfter(
         entity.myAction
       );
