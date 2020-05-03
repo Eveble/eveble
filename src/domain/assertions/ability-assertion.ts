9a1d13215add9335732bdc1328558c6b1f2ff365
Adam Desivi <desivi@desivi.com>
2020-05-03 04:19:06 +0200

    docs: add remarks to AbilityAssertion

--- src/domain/assertions/ability-assertion.ts
+++ src/domain/assertions/ability-assertion.ts
@@ -12,9 +12,30 @@ export class AbilityAssertion extends Assertion {
    * entity.ensure.is.ableTo.doAction(...)
    *```
    * We need to use Proxy to pass-through all calls to the entity itself
-   * (entity instance will be not set upon assertion registration)
+   * (entity state will be not changed upon invoking method).
+   * @remarks
+   * The `entity.ensure` getter-method will return a Proxified instance of the
+   * `Entity`. This proxified instance listens to all get methods and
+   * catches the requested method name.
+   *
+   * If the requested get method is named `is` - `is` an existing api registered
+   * on `Asserter` with `AbilityAssertion` as assertion API.
+   * This will return an object that will include property:
+   *```ts
+   * {ableTo: ...}
+   *```
+   * That will be fired with code below.
+   * @remarks
+   * Same approach is used for evaluator api `is.ableTo`(returning boolean
+   * by catching any thrown error) - however it was replaced by
+   * `Entity.prototype.can` method directly build on Entity.
+   * The `is.ableTo` legacy code is still available to show possibilities of
+   * building custom assertion apis without direct modification of
+   * `Entity` or its subclasses - to have a assertion api code shared
+   * 'globally'.
    */
   public api: Map<string, any> = new Map([
+    // Validation - ensures that `Entity` is able to handle an action(throws error)
     [
       'ensure.is.ableTo',
       new Proxy(this, {
@@ -36,6 +57,7 @@ export class AbilityAssertion extends Assertion {
         },
       }),
     ],
+    // Evaluation - evaluates that `Entity` is able to handle an action(returns boolean)
     [
       'is.ableTo',
       new Proxy(this, {
@@ -57,6 +79,7 @@ export class AbilityAssertion extends Assertion {
             });
             return proxifiedMethod;
           }
+          return entity[propKey];
         },
       }),
     ],
