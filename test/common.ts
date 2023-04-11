710a61654c32c1a8e4c0920a70cf6399682a9210
Adam Desivi <desivi@desivi.com>
2023-04-11 04:09:25 +0200

    refactor: use import on common.ts

--- test/common.ts
+++ test/common.ts
@@ -1,10 +1,12 @@
+import dotEnvExtended from 'dotenv-extended';
+
 const env = process.env.NODE_ENV;
 const envFile = process.env.NODE_ENV ? `.env.${env}` : '.env';
 /**
  * Assigns environment variables based on environment.
  */
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-require('dotenv-extended').load({
+dotEnvExtended.load({
   silent: false,
   defaults: '.env.defaults',
   schema: '.env.schema',
